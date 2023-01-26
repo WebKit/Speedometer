cbe176c0167ad33e2f318616b55ddde08ff73124
Brian Grinstead <briangrinstead@gmail.com>
2023-01-26 14:54:52 -0800

    document running the test

--- tests/run.mjs
+++ tests/run.mjs
@@ -2,6 +2,16 @@ import serve from "./server.mjs";
 import { Builder, Capabilities } from "selenium-webdriver";
 import assert from "assert";
 
+// To run this locally you'll need the browsers installed along with the corresponding driver:
+// - chromedriver (https://chromedriver.chromium.org/getting-started)
+// - geckodriver (https://github.com/mozilla/geckodriver/releases)
+// - safaridriver (https://developer.apple.com/documentation/webkit/testing_with_webdriver_in_safari).
+
+// Then you can run with the `BROWSER` environment variable, for example:
+// BROWSER=chrome node tests/unit-test-run.mjs
+// BROWSER=firefox node tests/unit-test-run.mjs
+// BROWSER=safari node tests/unit-test-run.mjs
+
 const PORT = 8010;
 const server = serve(PORT);
 
