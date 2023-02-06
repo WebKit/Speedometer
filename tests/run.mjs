019a588db6b4cee2c800cc2f065cad5f22f6b7ad
Camillo Bruni <cbruni@chromium.org>
2023-02-06 16:16:38 +0100

    fixing

--- tests/run.mjs
+++ tests/run.mjs
@@ -1,29 +1,76 @@
+#! /usr/bin/env node
+
+import { argv } from 'node:process';
 import serve from "./server.mjs";
 import { Builder, Capabilities } from "selenium-webdriver";
+import commandLineArgs  from "command-line-args";
+import commandLineUsage  from "command-line-usage";
 import assert from "assert";
 
-const PORT = 8010;
+
+const optionDefinitions = [
+    { name: "browser", type: String, defaultValue: "$BROWSER"},
+    { name: "port", type: Number, defaultValue: 8010},
+    { name: "help", alias: "h", description: "Print this help text."}
+];
+
+function printHelp(message = "")
+{
+    const usage = commandLineUsage([
+        {
+          header: 'Run all tests',
+        },
+        {
+          header: 'Options',
+          optionList: optionDefinitions
+        }
+      ])
+    if (!message) {
+        console.log(usage);
+        process.exit(0);
+    } else {
+        console.error(message)
+        console.error()
+        console.error(usage)
+        process.exit(1);
+    }
+}
+
+const options = commandLineArgs(optionDefinitions);
+
+if ("help" in options)
+    printHelp();
+
+
+const BROWSER = options.browser || process.env.BROWSER;
+if (!BROWSER)
+    printHelp("No browser specified, use $BROWSER or --browser")
+
+let capabilities;
+switch (BROWSER) {
+    case "safari":
+        capabilities = Capabilities.safari();
+        break;
+
+    case "firefox": {
+        capabilities = Capabilities.firefox();
+        break;
+    }
+    case "chrome": {
+        capabilities = Capabilities.chrome();
+        break;
+    }
+    default: {
+        printHelp(`Invalid browser "${BROWSER}", choices are: "safari", "firefox", "chrome"`);
+    }
+}
+
+const PORT = options.port;
 const server = serve(PORT);
 
 let driver;
 
 async function test() {
-    let capabilities;
-    switch (process.env.BROWSER) {
-        case "safari":
-            capabilities = Capabilities.safari();
-            break;
-
-        case "firefox": {
-            capabilities = Capabilities.firefox();
-            break;
-        }
-        case "chrome": {
-            capabilities = Capabilities.chrome();
-            break;
-        }
-    }
-
     driver = await new Builder().withCapabilities(capabilities).build();
 
     try {
