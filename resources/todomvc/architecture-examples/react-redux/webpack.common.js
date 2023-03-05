e37b196ac058d20850d6ca8b4dba4316336db2fc
Thorsten Kober <tkober@gmail.com>
2023-03-04 16:05:05 -0800

    fix build

--- resources/todomvc/architecture-examples/react-redux/webpack.common.js
+++ resources/todomvc/architecture-examples/react-redux/webpack.common.js
@@ -8,7 +8,6 @@ module.exports = {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-        publicPath: "/",
     },
     resolve: {
         extensions: [".js", ".jsx"],
