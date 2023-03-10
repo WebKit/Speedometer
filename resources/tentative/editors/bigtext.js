838e4841137934bcfaf226486a05626dd4de8ba1
Brian Grinstead <briangrinstead@gmail.com>
2023-03-09 16:22:57 -0800

    remove first line in code sample since the license comment is usually the first line

--- resources/tentative/editors/bigtext.js
+++ resources/tentative/editors/bigtext.js
@@ -1,6 +1,5 @@
 // import { code as smallCode, text as smallText } from "./smalltext.js";
-export const code = `
-/*
+export const code = `/*
  * Copyright (C) 2012, 2013 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
