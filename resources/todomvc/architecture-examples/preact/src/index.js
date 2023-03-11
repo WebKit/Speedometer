1e6842ec78fcb7c04dfc5a27779607c4b8717e8c
Thorsten Kober <tkober@gmail.com>
2023-03-10 18:18:44 -0800

    add devtools

--- resources/todomvc/architecture-examples/preact/src/index.js
+++ resources/todomvc/architecture-examples/preact/src/index.js
@@ -2,6 +2,8 @@ import { render } from "preact";
 import Router from 'preact-router';
 import { createHashHistory  } from 'history';
 
+import "preact/debug";
+
 import { App } from "./todo/app";
 
 import "todomvc-app-css/index.css";
