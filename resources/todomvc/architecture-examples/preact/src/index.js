67fe080a8a43c91e75c53df27237469d62c4a4d9
Thorsten Kober <tkober@gmail.com>
2023-03-14 16:14:42 -0700

    rollup cleanup

--- resources/todomvc/architecture-examples/preact/src/index.js
+++ resources/todomvc/architecture-examples/preact/src/index.js
@@ -2,5 +2,5 @@ import { h, render } from 'preact';
 import App from './app';
 // import 'todomvc-common';
 // import 'todomvc-common/base.css';
-// import 'todomvc-app-css/index.css';
+import 'todomvc-app-css/index.css';
 render(<App />, document.querySelector('.todoapp'));
