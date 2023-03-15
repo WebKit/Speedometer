1933672b810ea210c4087e90c79a9e06c3a30331
Thorsten Kober <tkober@gmail.com>
2023-03-15 08:27:53 -0700

    Todomvc preact - Phase 1 (#104)

--- resources/todomvc/architecture-examples/preact/README.md
+++ resources/todomvc/architecture-examples/preact/README.md
@@ -1,16 +1,35 @@
-# Speedometer 2.1: Preact TodoMVC example
+# Speedometer 3.0: TodoMVC: Preact
 
-## Test locally
+## Description
 
-1. `npm install`
-2. `npm start`
+This application uses Preact 10.11.3 to implement a todo application.
 
-## Build
+-   [Preact](https://preactjs.com/) Fast 3kB alternative to React with the same modern API.
 
-1. `npm run build`
+## Implementation details
 
-## Notes
+[TBD]
 
-The compiled version of the application can be found in the `build` directory.
-This is what the benchmark runner uses when running the Preact TodoMVC as part
-of a benchmark run.
+## Built steps
+
+```
+terminal:
+1. npm run build
+```
+
+## Requirements
+
+The only requirement is an installation of Node, to be able to install dependencies and run scripts to serve a local server.
+
+```
+* Node (min version: 18.13.0)
+* NPM (min version: 8.19.3)
+```
+
+## Local preview
+
+```
+terminal:
+1. npm install
+2. npm run start
+```
--- resources/todomvc/architecture-examples/preact/rollup.config.js
+++ resources/todomvc/architecture-examples/preact/rollup.config.js
@@ -1,27 +1,52 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import terser from "@rollup/plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import css from "rollup-plugin-import-css";
+import copy from "rollup-plugin-copy";
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
-    entry: 'src/index.js',
-    dest: 'dist/app.js',
-    format: 'iife',
-    sourceMap: true,
-    external: [],
+    input: "src/index.js",
+    output: [
+        {
+            file: "dist/app.js",
+            format: "iife",
+            sourcemap: true,
+        },
+    ],
     plugins: [
+        css({
+            minify: true,
+        }),
         babel({
             babelrc: false,
             presets: [
-                ['es2015', { loose:true, modules:false }],
-                'stage-0'
+                ["@babel/preset-env", { targets: "defaults" }],
+                ["@babel/preset-react", { runtime: "automatic" }],
             ],
             plugins: [
-                ['transform-react-jsx', { pragma:'h' }]
-            ]
+                [
+                    "@babel/plugin-transform-react-jsx",
+                    {
+                        pragma: "h",
+                        pragmaFrag: "Fragment",
+                    },
+                ],
+            ],
         }),
-        nodeResolve({
-            jsnext: true
+        resolve({
+            jsnext: true,
+            main: true,
+            browser: true,
         }),
-        commonjs()
-    ]
+        commonjs(),
+        copy({
+            targets: [{ src: "public/index.html", dest: "dist/" }],
+        }),
+        production && terser(),
+    ],
 };
