e2be5dc8aaec837ff7d6eb82901b2dbbfaf3ee1b
Thorsten Kober <tkober@gmail.com>
2023-03-15 14:05:54 -0700

    item function component

--- resources/todomvc/architecture-examples/preact/src/app/item.js
+++ resources/todomvc/architecture-examples/preact/src/app/item.js
@@ -1,52 +1,48 @@
 import cx from "classnames";
 // eslint-disable-next-line no-unused-vars
-import { h, Component } from "preact";
-export default class TodoItem extends Component {
-    handleSubmit = (e) => {
-        const { onSave, onRemove, todo } = this.props,
-            val = e.target.value.trim();
+import { h } from "preact";
+import { useState } from "preact/hooks";
+export default function TodoItem({ onSave, onRemove, onToggle, todo }) {
+    const [editing, setEditing] = useState(false);
+
+    function handleSubmit(e) {
+        const val = e.target.value.trim();
         if (val) {
             onSave(todo, val);
-            this.setState({ editing: false });
+            setEditing(false);
         } else {
             onRemove(todo);
         }
-    };
+    }
 
-    handleDoubleClick = () => {
-        this.setState({ editing: true });
-    };
+    function handleDoubleClick() {
+        setEditing(true);
+    }
 
-    handleToggle = (e) => {
-        const { onToggle, todo } = this.props;
+    function handleToggle(e) {
         onToggle(todo);
         e.preventDefault();
-    };
-
-    handleKeyDown = (e) => {
-        if (e.key === "Escape" || e.key === "ESCAPE"){
-            const { todo } = this.props;
-            this.setState({ editing: false });
-            this.props.onCancel(todo);
-        } else if (e.key === "Enter" || e.key === "ENTER") {
-            this.handleSubmit(e);
-        }
-    };
-
-    handleDestroy = () => {
-        this.props.onRemove(this.props.todo);
-    };
-
-    render({ todo: { title, completed } }, { editing }) {
-        return (
-            <li class={cx({ completed, editing })}>
-                <div class="view">
-                    <input class="toggle" type="checkbox" checked={completed} onChange={this.handleToggle} />
-                    <label onDblClick={this.handleDoubleClick}>{title}</label>
-                    <button class="destroy" onClick={this.handleDestroy} />
-                </div>
-                {editing && <input class="edit" onBlur={this.handleSubmit} onKeyDown={this.handleKeyDown} autoFocus defaultValue={title} />}
-            </li>
-        );
     }
+
+    function handleKeyDown(e) {
+        if (e.key === "Escape" || e.key === "ESCAPE")
+            setEditing(false);
+        else if (e.key === "Enter" || e.key === "ENTER")
+            handleSubmit(e);
+    }
+
+    function handleDestroy() {
+        onRemove(todo);
+    }
+
+    return (
+        <li class={cx({ completed: todo.completed, editing })}>
+            <div class="view">
+                <input class="toggle" type="checkbox" checked={todo.completed} onChange={handleToggle} />
+                <label onDblClick={handleDoubleClick}>{todo.title}</label>
+                <button class="destroy" onClick={handleDestroy} />
+            </div>
+            {editing && <input class="edit" onBlur={handleSubmit} onKeyDown={handleKeyDown} autoFocus defaultValue={todo.title} />}
+        </li>
+    );
 }
