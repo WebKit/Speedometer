928451fc4bc809184ef64fb2d44735ea69541c31
Thorsten Kober <tkober@gmail.com>
2023-03-14 19:15:50 -0700

    footer functional component

--- resources/todomvc/architecture-examples/preact/src/app/footer.js
+++ resources/todomvc/architecture-examples/preact/src/app/footer.js
@@ -1,40 +1,38 @@
 import cx from "classnames";
 // eslint-disable-next-line no-unused-vars
-import { h, Component } from "preact";
+import { h } from "preact";
 
 function pluralize(count, word) {
     return count === 1 ? word : `${word}s`;
 }
-export default class TodoFooter extends Component {
-    render({ nowShowing, count, completedCount, onClearCompleted }) {
-        return (
-            <footer class="footer">
-                <span class="todo-count">
-                    <strong>{count}</strong> {pluralize(count, "item")} left
-                </span>
-                <ul class="filters">
-                    <li>
-                        <a href="#/" class={cx({ selected: nowShowing === "all" })}>
-                            All
-                        </a>
-                    </li>{" "}
-                    <li>
-                        <a href="#/active" class={cx({ selected: nowShowing === "active" })}>
-                            Active
-                        </a>
-                    </li>{" "}
-                    <li>
-                        <a href="#/completed" class={cx({ selected: nowShowing === "completed" })}>
-                            Completed
-                        </a>
-                    </li>
-                </ul>
-                {completedCount > 0 && (
-                    <button class="clear-completed" onClick={onClearCompleted}>
-                        Clear completed
-                    </button>
-                )}
-            </footer>
-        );
-    }
+export default function TodoFooter({ nowShowing, count, completedCount, onClearCompleted }) {
+    return (
+        <footer class="footer">
+            <span class="todo-count">
+                <strong>{count}</strong> {pluralize(count, "item")} left
+            </span>
+            <ul class="filters">
+                <li>
+                    <a href="#/" class={cx({ selected: nowShowing === "all" })}>
+                        All
+                    </a>
+                </li>{" "}
+                <li>
+                    <a href="#/active" class={cx({ selected: nowShowing === "active" })}>
+                        Active
+                    </a>
+                </li>{" "}
+                <li>
+                    <a href="#/completed" class={cx({ selected: nowShowing === "completed" })}>
+                        Completed
+                    </a>
+                </li>
+            </ul>
+            {completedCount > 0 && (
+                <button class="clear-completed" onClick={onClearCompleted}>
+                    Clear completed
+                </button>
+            )}
+        </footer>
+    );
 }
