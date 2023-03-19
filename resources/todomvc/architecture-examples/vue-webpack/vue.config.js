24987c700ce188649c0d61a7e10fad573c69fc8a
Thorsten Kober <tkober@gmail.com>
2023-03-19 11:23:40 -0700

    config

--- resources/todomvc/architecture-examples/vue-webpack/vue.config.js
+++ resources/todomvc/architecture-examples/vue-webpack/vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: ""
+  publicPath: "",
+  terser: {
+    minify: 'uglifyJs',
+    // minify: "terser",
+    terserOptions: {
+        compress: {
+            drop_console: true,
+        },
+    },
+  },
 })
