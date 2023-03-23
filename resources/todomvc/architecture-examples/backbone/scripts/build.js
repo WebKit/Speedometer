3fefc6dabda04716de423d3ee0bb6974811b9af3
Camillo Bruni <cbruni@chromium.org>
2023-03-23 15:46:36 +0100

    revert some auto-formatting

--- resources/todomvc/architecture-examples/backbone/scripts/build.js
+++ resources/todomvc/architecture-examples/backbone/scripts/build.js
@@ -21,7 +21,8 @@ const build = async () => {
 
     // copy src folder
     await fs.cp(sourceDirectory, targetDirectory, { recursive: true }, (err) => {
-        if (err) console.error(err);
+        if (err)
+            console.error(err);
     });
 
     // copy html file
