ea7840746b930bc3bb018d54c3221bc92855c473
Thorsten Kober <tkober@gmail.com>
2023-03-24 07:30:10 -0700

    opt-in ember for format

--- resources/todomvc/architecture-examples/emberjs/app/components/footer.js
+++ resources/todomvc/architecture-examples/emberjs/app/components/footer.js
@@ -1,6 +1,6 @@
-import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
 
 export default class FooterComponent extends Component {
-  @service('todo-data') todos;
+    @service("todo-data") todos;
 }
--- resources/todomvc/architecture-examples/emberjs/app/routes/completed.js
+++ resources/todomvc/architecture-examples/emberjs/app/routes/completed.js
@@ -1,16 +1,16 @@
-import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
 export default class CompletedRoute extends Route {
-  @service('todo-data') todos;
+    @service("todo-data") todos;
 
-  model() {
-    const todos = this.todos;
+    model() {
+        const todos = this.todos;
 
-    return {
-      get completedItems() {
-        return todos.completedItems;
-      },
-    };
-  }
+        return {
+            get completedItems() {
+                return todos.completedItems;
+            },
+        };
+    }
 }
