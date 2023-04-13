e79d81f643884a3e23fc6bb06b371ab973f416fa
Thorsten Kober <tkober@gmail.com>
2023-04-12 18:15:20 -0700

    remove form model

--- resources/todomvc/architecture-examples/angular/src/app/todo-header/todo-header.component.ts
+++ resources/todomvc/architecture-examples/angular/src/app/todo-header/todo-header.component.ts
@@ -1,5 +1,4 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { Todo } from '../todo';
 import { TodosService } from '../todos.service';
 
 @Component({
@@ -10,14 +9,11 @@ import { TodosService } from '../todos.service';
 export class TodoHeaderComponent {
   constructor(private todosService: TodosService) {}
 
-  newTodo: Todo = new Todo("", "", false);
-
-  addTodo() {
-    const todoTitle = this.newTodo.title.trim();
+  addTodo(title: string) {
+    const todoTitle = title.trim();
     if (todoTitle.length <= 0)
       return;
 
     this.todosService.addItem(todoTitle);
-    this.newTodo.title = "";
   }
 }
