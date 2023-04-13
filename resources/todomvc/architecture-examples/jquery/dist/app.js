08dca7fa3ad6aac705ba0d9d503ac78112b3dfd8
Thorsten Kober <tkober@gmail.com>
2023-04-13 11:45:24 -0700

    revert render

--- resources/todomvc/architecture-examples/jquery/dist/app.js
+++ resources/todomvc/architecture-examples/jquery/dist/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-case-declarations */
 /* eslint no-unused-vars: 0 */
 /* eslint no-undef: 0 */
 /* eslint prefer-template: 0 */
@@ -11,20 +10,104 @@ jQuery(function ($) {
     });
 
     var App = {
-        getStats: function () {
-            const stats = { all: this.todos.length, active: 0, completed: 0 };
-            this.todos.forEach(function (todo) {
-                // eslint-disable-next-line no-unused-expressions
-                todo.completed ? stats.completed++ : stats.active++;
-            });
-            return stats;
-        },
-        getCurrentTodos: function () {
-            if (this.filter === "all")
-                return this.todos;
+        init: function () {
+            this.todos = [];
+            this.todoTemplate = Handlebars.compile($("#todo-template").html());
+            this.footerTemplate = Handlebars.compile($("#footer-template").html());
+            this.bindEvents();
 
-            const currentTodos = this.todos.filter((todo) => this.filter === "active" ? !todo.completed : todo.completed);
-            return currentTodos;
+            router(route => {
+                this.filter = route;
+                this.render();
+            }).init();
+
+            const dummyNodeToNotifyAppIsReady = document.createElement("div");
+            dummyNodeToNotifyAppIsReady.id = "appIsReady";
+            document.body.appendChild(dummyNodeToNotifyAppIsReady);
+        },
+        bindEvents: function () {
+            $("#new-todo").on("keyup", this.handleTodoInput.bind(this));
+            $("#toggle-all").on("change", this.handleToggleAll.bind(this));
+            $("#footer").on("click", ".clear-completed", this.handleRevoveCompleted.bind(this));
+            $("#todo-list")
+                .on("change", ".toggle", this.handleTodoToggle.bind(this))
+                .on("dblclick", "label", this.handleTodoEditStart.bind(this))
+                .on("keyup", ".edit", this.handleTodoEditStop.bind(this))
+                .on("focusout", ".edit", this.handleTodoUpdate.bind(this))
+                .on("click", ".destroy", this.handleTodoButton.bind(this));
+        },
+        render: function () {
+            $("#main").toggle(this.todos.length > 0);
+            $("#footer").toggle(this.todos.length > 0);
+
+            const currentTodos = this.getFilteredTodos();
+
+            if (this.todos.length > 0) {
+                $("#todo-list").html(this.todoTemplate(currentTodos));
+                $("#toggle-all").prop("checked", this.getActiveTodos().length === 0);
+
+                this.renderFooter();
+            }
+
+            $("#new-todo").trigger("focus");
+        },
+        renderFooter: function () {
+            var todoCount = this.todos.length;
+            var activeTodoCount = this.getActiveTodos().length;
+            var template = this.footerTemplate({
+                activeTodoCount: activeTodoCount,
+                activeTodoWord: pluralize(activeTodoCount, "item"),
+                completedTodos: todoCount - activeTodoCount,
+                filter: this.filter,
+            });
+
+            $("#footer").html(template);
+        },
+        getActiveTodos: function () {
+            return this.todos.filter(function (todo) {
+                return !todo.completed;
+            });
+        },
+        getCompletedTodos: function () {
+            return this.todos.filter(function (todo) {
+                return todo.completed;
+            });
+        },
+        getFilteredTodos: function () {
+            if (this.filter === "active")
+                return this.getActiveTodos();
+
+            if (this.filter === "completed")
+                return this.getCompletedTodos();
+
+            return this.todos;
+        },
+        handleToggleAll: function (e) {
+            var isChecked = $(e.target).prop("checked");
+
+            this.todos.forEach(function (todo) {
+                todo.completed = isChecked;
+            });
+
+            this.render();
+        },
+        handleRevoveCompleted: function () {
+            this.todos = this.getActiveTodos();
+            this.render();
+        },
+        // accepts an element from inside the `.item` div and
+        // returns the corresponding index in the `todos` array
+        indexFromEl: function (el) {
+            const id = $(el).closest("li").data("id");
+            const todos = this.todos;
+            let i = todos.length;
+
+            while (i--) {
+                if (todos[i].id === id)
+                    return i;
+            }
+
+            return -1;
         },
         handleTodoInput: function (e) {
             if (e.key !== "Enter")
@@ -32,24 +115,20 @@ jQuery(function ($) {
 
             const $input = $(e.target);
             const text = $input.val().trim();
-            this.update("addTodo", { title: text });
+
+            this.todos.push({
+                id: uuid(),
+                title: text,
+                completed: false,
+            });
 
             $input.val("");
-        },
-        handleToggleAll: function (e) {
-            var isChecked = $(e.target).prop("checked");
-            this.update("toggleAll", { completed: isChecked });
-        },
-        handleClearButton: function () {
-            this.update("removeCompleted");
+            this.render();
         },
         handleTodoToggle: function (e) {
-            const id = $(e.target).closest("li").data("id");
-            this.update("toggleTodo", { id });
-        },
-        handleTodoButton: function (e) {
-            const id = $(e.target).closest("li").data("id");
-            this.update("removeTodo", { id });
+            const index = this.indexFromEl(e.target);
+            this.todos[index].completed = !this.todos[index].completed;
+            this.render();
         },
         handleTodoEditStart: function (e) {
             const $input = $(e.target).closest("li").addClass("editing").find(".edit");
@@ -81,128 +160,12 @@ jQuery(function ($) {
                 return;
             }
 
-            const id = $(e.target).closest("li").data("id");
-            this.update("updateTodo", { id, title: text });
+            this.todos[this.indexFromEl(e.target)].title = text;
+            this.render();
         },
-        update: function (action, payload = {}) {
-            let todo = undefined;
-
-            switch (action) {
-                case "addTodo":
-                    todo = {
-                        id: uuid(),
-                        title: payload.title,
-                        completed: false,
-                    };
-                    this.todos.push(todo);
-                    this.render("addItem", todo);
-                    break;
-                case "toggleAll":
-                    this.todos.forEach(function (todo) {
-                        todo.completed = payload.completed;
-                    });
-                    this.render("updateList");
-                    break;
-                case "removeCompleted":
-                    this.todos = this.todos.filter((todo) => !todo.completed);
-                    this.render("updateList");
-                    break;
-                case "toggleTodo":
-                    todo = this.todos.find((todo) => todo.id === payload.id);
-                    if (todo !== undefined) {
-                        todo.completed = !todo.completed;
-                        this.render("toggleItem", todo);
-                    }
-                    break;
-                case "removeTodo":
-                    this.todos = this.todos.filter((todo) => todo.id !== payload.id);
-                    this.render("removeItem", payload);
-                    break;
-                case "updateTodo":
-                    todo = this.todos.find((todo) => todo.id === payload.id);
-                    if (todo !== undefined) {
-                        todo.title = payload.title;
-                        this.render("updateItem", todo);
-                    }
-                    break;
-            }
-        },
-        render: function (action, payload = {}) {
-            $("#main").toggle(this.todos.length > 0);
-            $("#footer").toggle(this.todos.length > 0);
-
-            const stats = this.getStats();
-            const currentTodos = this.getCurrentTodos();
-
-            switch (action) {
-                case "addItem":
-                    if (this.filter !== "completed")
-                        this.renderNewItem("#todo-list", payload);
-                    break;
-                case "toggleItem":
-                case "updateItem":
-                    this.renderUpdatedItem(payload);
-                    break;
-                case "removeItem":
-                    $(`[data-id="${payload.id}"]`).remove();
-                    break;
-                case "updateList":
-                    this.renderList("#todo-list", currentTodos);
-                    break;
-            }
-
-            if (this.todos.length > 0) {
-                this.renderFooter("#footer", stats);
-                $("#toggle-all").prop("checked", currentTodos.length > 0 && stats.active === 0);
-                $("#toggle-all").prop("disabled", currentTodos.length === 0);
-            }
-
-            $("#new-todo").trigger("focus");
-        },
-        renderNewItem: function (id, todo) {
-            $(id).append(this.todoTemplate(todo));
-        },
-        renderUpdatedItem: function (todo) {
-            $(`[data-id="${todo.id}"]`).replaceWith(this.todoTemplate(todo));
-        },
-        renderList: function (id, currentTodos) {
-            $(id).html(this.todoTemplate(currentTodos));
-        },
-        renderFooter: function (id, stats) {
-            const template = this.footerTemplate({
-                activeTodoCount: stats.active,
-                activeTodoWord: pluralize(stats.active, "item"),
-                completedTodos: stats.completed,
-                filter: this.filter,
-            });
-
-            $(id).html(template);
-        },
-        bindEvents: function () {
-            $("#new-todo").on("keyup", this.handleTodoInput.bind(this));
-            $("#toggle-all").on("change", this.handleToggleAll.bind(this));
-            $("#footer").on("click", ".clear-completed", this.handleClearButton.bind(this));
-            $("#todo-list")
-                .on("change", ".toggle", this.handleTodoToggle.bind(this))
-                .on("click", ".destroy", this.handleTodoButton.bind(this))
-                .on("dblclick", "label", this.handleTodoEditStart.bind(this))
-                .on("keyup", ".edit", this.handleTodoEditStop.bind(this))
-                .on("focusout", ".edit", this.handleTodoUpdate.bind(this));
-        },
-        init: function () {
-            this.todos = [];
-            this.todoTemplate = Handlebars.compile($("#todo-template").html());
-            this.footerTemplate = Handlebars.compile($("#footer-template").html());
-            this.bindEvents();
-
-            router((route) => {
-                this.filter = route;
-                this.render("updateList");
-            }).init();
-
-            var dummyNodeToNotifyAppIsReady = document.createElement("div");
-            dummyNodeToNotifyAppIsReady.id = "appIsReady";
-            document.body.appendChild(dummyNodeToNotifyAppIsReady);
+        handleTodoButton: function (e) {
+            this.todos.splice(this.indexFromEl(e.target), 1);
+            this.render();
         },
     };
 
