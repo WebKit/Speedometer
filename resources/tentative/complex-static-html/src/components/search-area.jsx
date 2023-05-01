7ff1d50d1685897cbe681351da859b42990e11c2
Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
2023-05-01 13:17:19 -0700

    Fixed search area

--- resources/tentative/complex-static-html/src/components/search-area.jsx
+++ resources/tentative/complex-static-html/src/components/search-area.jsx
@@ -5,7 +5,7 @@ export const SearchArea = () => {
   return (
     <form className="spectrum-Search">
       <div className="spectrum-Textfield spectrum-Search-textfield">
-        <SearchIcon className="spectrum-Icon spectrum-Icon--sizeM spectrum-Textfield-icon spectrum-Search-icon" focusable="false" aria-hidden="true"/>
+        <SearchIcon className="spectrum-Icon spectrum-Icon--sizeM spectrum-Textfield-icon spectrum-Search-icon" focusable="false" aria-hidden="true" id="spectrum-icon-18-Magnify"/>
         <input type="search" placeholder="Search" name="search" defaultValue="" className="spectrum-Textfield-input spectrum-Search-input" autoComplete="off"/>
       </div>
       <button type="reset" className="spectrum-ClearButton spectrum-ClearButton--sizeM spectrum-Search-clearButton">
