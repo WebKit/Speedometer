de0953aa15831674bafc506013b9e2e4ea00fe4d
Thorsten Kober <tkober@gmail.com>
2023-05-07 07:58:57 -0700

    scrollTo

--- resources/tentative/newssite/news-next/src/partials/layout/Layout.jsx
+++ resources/tentative/newssite/news-next/src/partials/layout/Layout.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+
 import Header from "../header/Header";
 import Navigation from "../navigation/Navigation";
 import Main from "../main/Main";
 import Footer from "../footer/Footer";
 
 export default function Layout({ children }) {
+    const pageRef = useRef(null);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        pageRef?.current?.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "instant",
+        });
+    }, [pathname]);
+
     return (
         <>
-            <div className="page">
+            <div className="page" ref={pageRef}>
                 <Header />
                 <Navigation />
                 <Main>
