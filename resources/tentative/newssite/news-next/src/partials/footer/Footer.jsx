d2ef4f2e68c93142e84589c42c8efb67eb2e6e06
Thorsten Kober <tkober@gmail.com>
2023-05-09 15:20:24 -0700

    social icons footer

--- resources/tentative/newssite/news-next/src/partials/footer/Footer.jsx
+++ resources/tentative/newssite/news-next/src/partials/footer/Footer.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { createPortal } from "react-dom";
 import Dialog from "@/components/dialog/Dialog";
 import ReducedMotion from "@/assets/ReducedMotion";
+import Facebook from "@/assets/Facebook";
+import Instagram from "@/assets/Instagram";
+import Twitter from "@/assets/Twitter";
 
 export default function Footer() {
     const [showSettings, setShowSettings] = useState(false);
@@ -18,6 +21,33 @@ export default function Footer() {
         <>
             <footer className="page-footer">
                 <div className="footer-row">
+                    <div className="footer-column-left">
+                        <div className="footer-icons">
+                            <ul className="footer-icons-list">
+                                <li className="footer-icons-item">
+                                    <a href="#" id="footer-link-social-facebook">
+                                        <div className="footer-icon">
+                                            <Facebook />
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className="footer-icons-item">
+                                    <a href="#" id="footer-link-social-instagram">
+                                        <div className="footer-icon">
+                                            <Instagram />
+                                        </div>
+                                    </a>
+                                </li>
+                                <li className="footer-icons-item">
+                                    <a href="#" id="footer-link-social-twitter">
+                                        <div className="footer-icon">
+                                            <Twitter />
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                     <div className="footer-column-center">© {new Date().getFullYear()} No Rights Reserved</div>
                     <div className="footer-column-right">
                         <div className="footer-icons">
@@ -34,7 +64,7 @@ export default function Footer() {
                     </div>
                 </div>
             </footer>
-            { showSettings ? createPortal(<Dialog onClose={closeDialog} />, document.getElementById("settings-container")) : null }
+            {showSettings ? createPortal(<Dialog onClose={closeDialog} />, document.getElementById("settings-container")) : null}
         </>
     );
 }
