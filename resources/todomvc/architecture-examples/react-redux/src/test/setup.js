5cf8184de4bcdd934d28e6d5b515215b8e5c1b7c
Thorsten Kober <tkober@gmail.com>
2023-05-19 16:40:57 -0700

    [TodoMVC]: replace crypto.randomUUID (#168)

--- resources/todomvc/architecture-examples/react-redux/src/test/setup.js
+++ resources/todomvc/architecture-examples/react-redux/src/test/setup.js
@@ -1,9 +1,1 @@
 import "@testing-library/jest-dom/extend-expect";
-
-import crypto from "crypto";
-
-Object.defineProperty(globalThis, "crypto", {
-    value: {
-        randomUUID: () => crypto.randomUUID(),
-    },
-});
