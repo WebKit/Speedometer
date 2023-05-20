784ea041ae278f32642b275226c2dfb55361ce5a
Brian Grinstead <briangrinstead@gmail.com>
2023-05-19 17:05:02 -0700

    review feedback

--- resources/developer-mode.mjs
+++ resources/developer-mode.mjs
@@ -1,15 +1,15 @@
 import { Suites } from "./tests.mjs";
 
 export function createDeveloperModeContainer() {
-    let container = document.createElement("div");
+    const container = document.createElement("div");
     container.className = "developer-mode";
 
-    let details = document.createElement("details");
-    let summary = document.createElement("summary");
+    const details = document.createElement("details");
+    const summary = document.createElement("summary");
     summary.textContent = "Developer Mode";
     details.append(summary);
 
-    let content = document.createElement("div");
+    const content = document.createElement("div");
     content.className = "developer-mode-content";
     content.append(createUIForSuites());
     details.append(content);
@@ -23,23 +23,26 @@ export function createUIForSuites() {
     const ol = document.createElement("ol");
     const checkboxes = [];
 
-    for (let suite of Suites) {
+    const setSuiteEnabled = (suiteIndex, enabled) => {
+        Suites[suiteIndex].disabled = !enabled;
+        checkboxes[suiteIndex].checked = enabled;
+    };
+
+    for (const suite of Suites) {
         const li = document.createElement("li");
         const checkbox = document.createElement("input");
         checkbox.id = suite.name;
         checkbox.type = "checkbox";
         checkbox.checked = !suite.disabled;
-        checkbox.onclick = (e) => {
+        checkbox.onclick = (event) => {
             suite.disabled = !checkbox.checked;
-            if (e?.ctrlKey || e?.metaKey) {
+            if (event?.ctrlKey || event?.metaKey) {
                 for (let suiteIndex = 0; suiteIndex < Suites.length; suiteIndex++) {
-                    if (Suites[suiteIndex] !== suite) {
-                        Suites[suiteIndex].disabled = true;
-                        checkboxes[suiteIndex].checked = false;
-                    } else {
-                        Suites[suiteIndex].disabled = false;
-                        checkboxes[suiteIndex].checked = true;
-                    }
+                    if (Suites[suiteIndex] !== suite)
+                        setSuiteEnabled(suiteIndex, false);
+                    else
+                        setSuiteEnabled(suiteIndex, true);
+
                 }
             }
             fixupURL();
@@ -47,7 +50,7 @@ export function createUIForSuites() {
         checkboxes.push(checkbox);
 
         li.appendChild(checkbox);
-        var label = document.createElement("label");
+        const label = document.createElement("label");
         label.appendChild(document.createTextNode(suite.name));
         li.appendChild(label);
         label.htmlFor = checkbox.id;
@@ -60,10 +63,9 @@ export function createUIForSuites() {
     let button = document.createElement("button");
     button.textContent = "Select all";
     button.onclick = () => {
-        for (let suiteIndex = 0; suiteIndex < Suites.length; suiteIndex++) {
-            Suites[suiteIndex].disabled = false;
-            checkboxes[suiteIndex].checked = true;
-        }
+        for (let suiteIndex = 0; suiteIndex < Suites.length; suiteIndex++)
+            setSuiteEnabled(suiteIndex, true);
+
         fixupURL(Suites);
     };
     control.appendChild(button);
@@ -71,10 +73,9 @@ export function createUIForSuites() {
     button = document.createElement("button");
     button.textContent = "Unselect all";
     button.onclick = () => {
-        for (let suiteIndex = 0; suiteIndex < Suites.length; suiteIndex++) {
-            Suites[suiteIndex].disabled = true;
-            checkboxes[suiteIndex].checked = false;
-        }
+        for (let suiteIndex = 0; suiteIndex < Suites.length; suiteIndex++)
+            setSuiteEnabled(suiteIndex, false);
+
         fixupURL();
     };
     control.appendChild(button);
@@ -85,14 +86,13 @@ export function createUIForSuites() {
 function fixupURL() {
     // If less than all suites are selected then change the URL "Suites" GET parameter
     // to comma separate only the selected
-    let selectedSuites = [];
+    const selectedSuites = [];
     for (let suiteIndex = 0; suiteIndex < Suites.length; suiteIndex++) {
         if (!Suites[suiteIndex].disabled)
             selectedSuites.push(Suites[suiteIndex].name);
-
     }
     if (selectedSuites.length === 0 || selectedSuites.length === Suites.length) {
-        let url = new URL(window.location.href);
+        const url = new URL(window.location.href);
         url.searchParams.delete("suites");
         url.searchParams.delete("suite");
         url.search = decodeURIComponent(url.search);
@@ -102,7 +102,7 @@ function fixupURL() {
             window.history.pushState({}, "", url);
 
     } else {
-        let url = new URL(window.location.href);
+        const url = new URL(window.location.href);
         url.searchParams.delete("suite");
         url.searchParams.set("suites", selectedSuites.join(","));
         url.search = decodeURIComponent(url.search);
