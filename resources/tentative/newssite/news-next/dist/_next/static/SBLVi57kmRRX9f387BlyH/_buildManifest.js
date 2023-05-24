d865855a8ff116ec0faaa36dc05ae785bd0b39fc
Thorsten Kober <tkober@gmail.com>
2023-05-24 08:11:35 -0700

    use home link ;

--- resources/tentative/newssite/news-next/dist/_next/static/SBLVi57kmRRX9f387BlyH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/40-d4f1ef926668e88b.js","static/chunks/pages/index-86c2106d0024cdaf.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
