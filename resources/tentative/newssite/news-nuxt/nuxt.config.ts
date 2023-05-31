d3d69ace06b93082959b4ab87d252bfd44a92188
Thorsten Kober <tkober@gmail.com>
2023-05-31 13:41:19 -0700

    use dist folder

--- resources/tentative/newssite/news-nuxt/nuxt.config.ts
+++ resources/tentative/newssite/news-nuxt/nuxt.config.ts
@@ -1,34 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const path = require('path');
-const development = process.env.NODE_ENV === "development";
+const path = require("path");
 
+// used for github pages
+// const development = process.env.NODE_ENV === "development";
+
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
     ssr: false,
-    /* router: {
-        options: {
-          hashMode: true
-        }
-    }, */
     css: [
-        /* '~/assets/styles/global.css', */
-        'news-site-css/dist/variables.css',
-        'news-site-css/dist/global.css',
-        'news-site-css/dist/a11y.css',
-        'news-site-css/dist/icons.css',
-        'news-site-css/dist/text.css',
+        "news-site-css/dist/variables.css",
+        "news-site-css/dist/global.css",
+        "news-site-css/dist/a11y.css",
+        "news-site-css/dist/icons.css",
+        "news-site-css/dist/text.css",
     ],
     components: [
-        '~/components',
-        '~/components/assets',
-        '~/components/atoms',
-        '~/components/molecules'
+        "~/components",
+        "~/components/assets",
+        "~/components/atoms",
+        "~/components/molecules"
     ],
     nitro: {
         output: {
-            publicDir: path.join(__dirname, 'docs')
+            // used for github pages
+            // publicDir: path.join(__dirname, "docs")
+            publicDir: path.join(__dirname, "dist")
         }
     },
     app: {
-        baseURL: development ? "" : '/news-site-nuxt-static',
+        // used for github pages
+        // baseURL: development ? "" : "/news-site-nuxt-static",
     }
-})
+});
