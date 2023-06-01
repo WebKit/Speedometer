85ddd7a0dd9364f81815fe5bee954bc60b81d341
Thorsten Kober <tkober@gmail.com>
2023-06-01 10:12:12 -0700

    generate new build

---
+++ resources/tentative/newssite/news-next/dist/_next/static/xDJUAmJJUUH4Bl0cmYduG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a0e07d66c78a79f7.css","static/chunks/958-173e7510442014ad.js","static/chunks/pages/index-b455787c6050bfa4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
