b1e3eeb1ba4605d92ba57568496ccae27f9659c1
Thorsten Kober <tkober@gmail.com>
2023-06-02 09:14:39 -0700

    Update resources/tentative/newssite/news-nuxt/nuxt.config.ts
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- resources/tentative/newssite/news-nuxt/nuxt.config.ts
+++ resources/tentative/newssite/news-nuxt/nuxt.config.ts
@@ -1,9 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const path = require("path");
+import path from "path";
+import { defineNuxtConfig } from "nuxt/config";
 
-const development = process.env.NODE_ENV === "development";
-
-// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
     ssr: false,
     css: ["news-site-css/dist/variables.css", "news-site-css/dist/global.css", "news-site-css/dist/a11y.css", "news-site-css/dist/icons.css", "news-site-css/dist/text.css"],
