b7fa02419e22dd916448df96026d7839ceef1574
Thorsten Kober <tkober@gmail.com>
2023-06-02 13:34:43 -0700

    Update resources/tentative/newssite/news-nuxt/app.vue
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- resources/tentative/newssite/news-nuxt/app.vue
+++ resources/tentative/newssite/news-nuxt/app.vue
@@ -1,15 +1,17 @@
 <script>
-// "Fixes" an issue with calling history.replaceState too often in certain browsers during testing.
-// Note: This should NOT be used in a production application.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-history.replaceState = function (state) {
-    return null;
-};
-
-// This hack allows to capture the work normally happening in a rAF. We
-// may be able to remove it if the runner improves.
-window.requestAnimationFrame = (cb) => window.setTimeout(cb, 0);
-window.cancelAnimationFrame = window.clearTimeout;
+if (process.client) {
+    // "Fixes" an issue with calling history.replaceState too often in certain browsers during testing.
+    // Note: This should NOT be used in a production application.
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    history.replaceState = function (state) {
+        return null;
+    };
+    
+    // This hack allows to capture the work normally happening in a rAF. We
+    // may be able to remove it if the runner improves.
+    window.requestAnimationFrame = (cb) => window.setTimeout(cb, 0);
+    window.cancelAnimationFrame = window.clearTimeout;
+}
 </script>
 
 <script setup>
