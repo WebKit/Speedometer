45fd59982b55977587b446eceb8c31164f68e2cb
Thorsten Kober <tkober@gmail.com>
2023-06-02 11:33:29 -0700

    move layout into app

--- resources/tentative/newssite/news-nuxt/app.vue
+++ resources/tentative/newssite/news-nuxt/app.vue
@@ -12,10 +12,57 @@ window.requestAnimationFrame = (cb) => window.setTimeout(cb, 0);
 window.cancelAnimationFrame = window.clearTimeout;
 </script>
 
+<script setup>
+import { nextTick, watch } from "#imports";
+import styles from "news-site-css/dist/layout.module.css";
+import { content } from "./data/content";
+
+const showMessage = ref(false);
+const route = useRoute();
+
+onMounted(() => {
+    showMessage.value = content[route.name].message;
+});
+
+const closeMessage = () => {
+    showMessage.value = false;
+};
+
+watch(
+    route,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (value) => {
+        if (document.getElementById("page")) {
+            if (!route.hash) {
+                document.getElementById("page").scrollTo(0, 0);
+            } else {
+                const elementId = route.hash.split("#")[1];
+                nextTick(() => {
+                    document.getElementById(elementId).scrollIntoView();
+                });
+            }
+        }
+    },
+    { deep: true, immediate: true }
+);
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-  <div id="settings-container" />
-  <div id="notifications-container" />
+  <div
+    id="page"
+    :class="styles.page"
+  >
+    <Header />
+    <Navigation />
+    <Message
+      v-if="content[route.name].message"
+      v-show="showMessage"
+      :on-close="closeMessage"
+      :message="content[route.name].message"
+    />
+    <Main>
+      <NuxtPage />
+    </Main>
+    <Footer />
+  </div>
 </template>
--- resources/tentative/newssite/news-nuxt/nuxt.config.ts
+++ resources/tentative/newssite/news-nuxt/nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import path from "path";
 import { defineNuxtConfig } from "nuxt/config";
+import { NuxtPage } from "nuxt/schema";
 
 export default defineNuxtConfig({
     ssr: false,
     css: ["news-site-css/dist/variables.css", "news-site-css/dist/global.css", "news-site-css/dist/a11y.css", "news-site-css/dist/icons.css", "news-site-css/dist/text.css"],
     components: ["~/components", "~/components/assets", "~/components/atoms", "~/components/molecules"],
     hooks: {
-        "pages:extend"(pages) {
+        "pages:extend"(pages: NuxtPage[]) {
             ["home", "us", "world", "politics", "business", "opinion", "health"].map((name) => {
                 pages.push({
                     name,
