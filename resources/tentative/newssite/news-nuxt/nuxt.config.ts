d803495ad7952fa1ce796c3fbb37fe686d159223
Thorsten Kober <tkober@gmail.com>
2023-06-02 09:20:12 -0700

    pr feedback

--- resources/tentative/newssite/news-nuxt/nuxt.config.ts
+++ resources/tentative/newssite/news-nuxt/nuxt.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
+    ssr: false,
     css: ["news-site-css/dist/variables.css", "news-site-css/dist/global.css", "news-site-css/dist/a11y.css", "news-site-css/dist/icons.css", "news-site-css/dist/text.css"],
     components: ["~/components", "~/components/assets", "~/components/atoms", "~/components/molecules"],
     nitro: {
