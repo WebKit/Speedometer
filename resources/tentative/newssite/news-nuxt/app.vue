39670cac29fc13eaabdca729148b28a645d1857a
Thorsten Kober <tkober@gmail.com>
2023-06-02 09:35:19 -0700

    remove meta from app.vue

--- resources/tentative/newssite/news-nuxt/app.vue
+++ resources/tentative/newssite/news-nuxt/app.vue
@@ -1,21 +1,3 @@
-<script setup>
-useHead({
-    title: "The Daily Broadcast",
-    htmlAttrs: {
-        lang: "en",
-    },
-    meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        {
-            hid: "description",
-            name: "description",
-            content: "A news site developed with Nuxt.",
-        },
-    ],
-});
-</script>
-
 <script>
   // "Fixes" an issue with calling history.replaceState too often in certain browsers during testing.
   // Note: This should NOT be used in a production application.
