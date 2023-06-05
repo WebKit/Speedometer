c3f820fa845bb5868c2f3ab1ebf2bd662090cb07
Thorsten Kober <tkober@gmail.com>
2023-06-05 15:52:52 -0700

    revert router change

--- resources/tentative/newssite/news-nuxt/nuxt.config.ts
+++ resources/tentative/newssite/news-nuxt/nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import path from "path";
 import { defineNuxtConfig } from "nuxt/config";
-import { NuxtPage } from "nuxt/schema";
+// import { NuxtPage } from "nuxt/schema";
 
 export default defineNuxtConfig({
     ssr: false,
     css: ["news-site-css/dist/variables.css", "news-site-css/dist/global.css", "news-site-css/dist/a11y.css", "news-site-css/dist/icons.css", "news-site-css/dist/text.css"],
     components: ["~/components", "~/components/assets", "~/components/atoms", "~/components/molecules"],
-    hooks: {
+    /* hooks: {
         "pages:extend"(pages: NuxtPage[]) {
             for (const name of ["home", "us", "world", "politics", "business", "opinion", "health"]) {
                 pages.push({
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
                 });
             }
         },
-    },
+    }, */
     nitro: {
         prerender: {
             crawlLinks: false
