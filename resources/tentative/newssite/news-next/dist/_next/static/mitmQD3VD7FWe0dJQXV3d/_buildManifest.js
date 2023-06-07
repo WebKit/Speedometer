684b6efa8b8486f0bd3bdbc0157f7e45ad1d079e
Thorsten Kober <tkober@gmail.com>
2023-06-07 11:19:34 -0700

    add css package locally

---
+++ resources/tentative/newssite/news-next/dist/_next/static/mitmQD3VD7FWe0dJQXV3d/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-fd706aeabb7828e3.js","static/css/738eeb1e0ba7c344.css","static/chunks/pages/index-7eb906afac4e0c07.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
