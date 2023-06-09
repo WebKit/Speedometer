064312fb80e8d1e9aee1a33b940b6aa64d149826
Julien Wajsberg <felash@gmail.com>
2023-06-09 12:11:36 +0200

    Move stockcharts out of tentative

--- resources/tests.mjs
+++ resources/tests.mjs
@@ -583,7 +583,7 @@ Suites.push({
 
 Suites.push({
     name: "React-Stockcharts-SVG",
-    url: "tentative/react-stockcharts/build/index.html?type=svg",
+    url: "react-stockcharts/build/index.html?type=svg",
     tags: ["chart", "svg"],
     async prepare(page) {
         await page.waitForElement("#render");
