387720d1a75a52f7e1072ca3ef8d1d52286bafcc
Thorsten Kober <tkober@gmail.com>
2023-06-13 14:49:52 -0700

    Newssite next.js format (#236)

--- resources/tentative/newssite/news-next/src/components/navlist/navlist-item.jsx
+++ resources/tentative/newssite/news-next/src/components/navlist/navlist-item.jsx
@@ -5,10 +5,7 @@ import styles from "news-site-css/dist/navbar.module.css";
 
 export default function NavListItem({ id, label, url, callback, itemClass }) {
     return (
-        <li className={classNames(
-            styles["navbar-item"],
-            itemClass
-        )} onClick={callback}>
+        <li className={classNames(styles["navbar-item"], itemClass)} onClick={callback}>
             <NavLink to={url} id={id} className={({ isActive }) => classNames({ [styles.active]: isActive })}>
                 {label}
             </NavLink>
--- resources/tentative/newssite/news-next/src/data/links.js
+++ resources/tentative/newssite/news-next/src/data/links.js
@@ -2,39 +2,39 @@ export const social = {
     facebook: {
         label: "Facebook",
         href: "#",
-        target: "external"
+        target: "external",
     },
     instagram: {
         label: "Instagram",
         href: "#",
-        target: "external"
+        target: "external",
     },
     twitter: {
         label: "Twitter",
         href: "#",
-        target: "external"
-    }
+        target: "external",
+    },
 };
 
 export const legal = {
     terms: {
         label: "Terms of Use",
         href: "#",
-        target: "external"
+        target: "external",
     },
     privacy: {
         label: "Privacy Policy",
         href: "#",
-        target: "external"
+        target: "external",
     },
     sell: {
         label: "Do Not Sell Or Share My Personal Information",
         href: "#",
-        target: "external"
+        target: "external",
     },
     choices: {
         label: "Ad Choices",
         href: "#",
-        target: "external"
-    }
+        target: "external",
+    },
 };
