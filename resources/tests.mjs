840394d69c7c2182fd060c74c5e263e30461e35e
Julien Wajsberg <felash@gmail.com>
2023-06-21 11:32:47 +0200

    Move the charts workload out of the tentative directory

--- resources/tests.mjs
+++ resources/tests.mjs
@@ -526,7 +526,7 @@ Suites.push({
 
 Suites.push({
     name: "Charts-observable-plot",
-    url: "tentative/charts/dist/observable-plot.html",
+    url: "charts/dist/observable-plot.html",
     tags: ["chart"],
     async prepare(page) {},
     tests: [
@@ -557,7 +557,7 @@ Suites.push({
 
 Suites.push({
     name: "Charts-chartjs",
-    url: "tentative/charts/dist/chartjs.html",
+    url: "charts/dist/chartjs.html",
     tags: ["chart"],
     async prepare(page) {},
     tests: [
