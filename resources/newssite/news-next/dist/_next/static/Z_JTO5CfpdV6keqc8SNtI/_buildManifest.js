18c51731a398a53c7ffe2e9c40ed5ea4d34a7112
Thorsten Kober <tkober@gmail.com>
2023-07-01 14:23:21 -0700

    add hover

---
+++ resources/newssite/news-next/dist/_next/static/Z_JTO5CfpdV6keqc8SNtI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-fd706aeabb7828e3.js","static/css/eb45e76eb4b52edd.css","static/chunks/pages/index-f1c407757a1dede3.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
