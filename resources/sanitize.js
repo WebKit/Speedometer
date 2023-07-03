4a4b58fef693b5380c5ac0f119aeda939d4473c4
Camillo Bruni <cbruni@chromium.org>
2023-07-03 19:01:46 +0200

    fix

--- resources/sanitize.js
+++ resources/sanitize.js
@@ -2,10 +2,18 @@
 // may be able to remove it if the runner improves.
 window.requestAnimationFrame = (cb) => window.setTimeout(cb, 0);
 window.cancelAnimationFrame = window.clearTimeout;
+
+// Only allow setTimeout(cn, 0) for consistency.
 const setTimeout = window.setTimeout;
 window.setTimeout = function (handler, timeout) {
     setTimeout(handler, 0);
 };
 
-window.requestIdleCallback = function (callback) {};
-window.cancelIdleCallback = function (id) {};
+// Don't support requestIdleCallback on any browser.
+window.requestIdleCallback = undefined;
+window.cancelIdleCallback = undefined;
+
+// Use a common userAgent string to minimize risk of running unfair 
+// browser-specific code in workloads.
+navigator.userAgent = "Firefox Safari Chrome";
+
