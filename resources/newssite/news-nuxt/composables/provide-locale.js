22588291cb2af0ecdb039918328440c421769956
Thorsten Kober <tkober@gmail.com>
2023-07-05 22:46:04 -0700

    add lang

--- resources/newssite/news-nuxt/composables/provide-locale.js
+++ resources/newssite/news-nuxt/composables/provide-locale.js
@@ -1,10 +1,24 @@
 import { provide } from "vue";
 import { useHead } from "#imports";
 import { content as contentEn } from "~/data/en/content";
+import { content as contentJp } from "~/data/jp/content";
+import { content as contentAr } from "~/data/ar/content";
+
 import { settings as settingsEn } from "~/data/en/dialog";
+import { settings as settingsJp } from "~/data/jp/dialog";
+import { settings as settingsAr } from "~/data/ar/dialog";
+
 import { footer as footerEn } from "~/data/en/footer";
+import { footer as footerJp } from "~/data/jp/footer";
+import { footer as footerAr } from "~/data/ar/footer";
+
 import * as buttonsEn from "~/data/en/buttons";
+import * as buttonsJp from "~/data/jp/buttons";
+import * as buttonsAr from "~/data/ar/buttons";
+
 import * as linksEn from "~/data/en/links";
+import * as linksJp from "~/data/jp/links";
+import * as linksAr from "~/data/ar/links";
 
 const strings = {
     en: {
@@ -13,6 +27,20 @@ const strings = {
         footer: footerEn,
         buttons: buttonsEn,
         links: linksEn,
+    },
+    jp: {
+        content: contentJp,
+        settings: settingsJp,
+        footer: footerJp,
+        buttons: buttonsJp,
+        links: linksJp,
+    },
+    ar: {
+        content: contentAr,
+        settings: settingsAr,
+        footer: footerAr,
+        buttons: buttonsAr,
+        links: linksAr,
     },
 };
 
