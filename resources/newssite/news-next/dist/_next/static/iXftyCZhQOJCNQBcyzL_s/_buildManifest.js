addd281ae5ffe618e5576077fee284ec1c4f5b61
Thorsten Kober <tkober@gmail.com>
2023-07-08 18:01:43 -0700

    merge main

--- resources/newssite/news-next/dist/_next/static/iXftyCZhQOJCNQBcyzL_s/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:resources/newssite/news-next/dist/_next/static/jgT0sxc49BQnfjC4EprI4/_buildManifest.js
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-fd706aeabb7828e3.js","static/css/2012df5addd38758.css","static/chunks/pages/index-9575ae3e903fa22e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
-========
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-fd706aeabb7828e3.js","static/css/8b914e4a9001ee1d.css","static/chunks/pages/index-90056c4c16843d2e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
->>>>>>>> main:resources/newssite/news-next/dist/_next/static/iXftyCZhQOJCNQBcyzL_s/_buildManifest.js
