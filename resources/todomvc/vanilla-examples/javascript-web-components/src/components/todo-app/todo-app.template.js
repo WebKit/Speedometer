95af4c495699762fbfbaefb7b5ab8c8db487980f
Issack John <117320405+issackjohn@users.noreply.github.com>
2023-07-10 13:35:42 -0700

    bug fix (#276)

--- resources/todomvc/vanilla-examples/javascript-web-components/src/components/todo-app/todo-app.template.js
+++ resources/todomvc/vanilla-examples/javascript-web-components/src/components/todo-app/todo-app.template.js
@@ -5,7 +5,7 @@ template.innerHTML = `
     <section class="app">
         <todo-topbar></todo-topbar>
         <main class="main">
-            <todo-list><todo-list>
+            <todo-list></todo-list>
         </main>
         <todo-bottombar></todo-bottombar>
     </section>
