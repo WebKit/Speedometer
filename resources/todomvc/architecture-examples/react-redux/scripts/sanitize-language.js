1a819d39ad456ca650986f989f1d9b9aaec09695
Thorsten Kober <tkober@gmail.com>
2023-07-12 15:28:31 -0700

    format sanitize script

--- resources/todomvc/architecture-examples/react-redux/scripts/sanitize-language.js
+++ resources/todomvc/architecture-examples/react-redux/scripts/sanitize-language.js
@@ -5,24 +5,23 @@ const dirName = "./dist";
 // spelling out words that this file doesn't trigger any bad word filters.
 const parts = [
     ["b", "l", "a", "c", "k", "l", "i", "s", "t"],
-    ["w", "h", "i", "t", "e", "l", "i", "s", "t"]
+    ["w", "h", "i", "t", "e", "l", "i", "s", "t"],
 ];
-const wordsToUse = [
-    "dissallowlist",
-    "allowlist"
-];
+const wordsToUse = ["dissallowlist", "allowlist"];
 
-const wordsToReplace = parts.map(part => part.join(""));
+const wordsToReplace = parts.map((part) => part.join(""));
 const replacements = new Map();
 wordsToReplace.forEach((word, index) => replacements.set(word, wordsToUse[index]));
 const stringReplaceRegex = new RegExp(wordsToReplace.join("|"), "gi");
 
 async function getFiles(dir) {
     const dirents = await fs.readdir(dir, { withFileTypes: true });
-    const files = await Promise.all(dirents.map((dirent) => {
-        const res = resolve(dir, dirent.name);
-        return dirent.isDirectory() ? getFiles(res) : res;
-    }));
+    const files = await Promise.all(
+        dirents.map((dirent) => {
+            const res = resolve(dir, dirent.name);
+            return dirent.isDirectory() ? getFiles(res) : res;
+        })
+    );
     return Array.prototype.concat(...files);
 }
 
--- resources/todomvc/architecture-examples/vue/scripts/sanitize-language.js
+++ resources/todomvc/architecture-examples/vue/scripts/sanitize-language.js
@@ -1,28 +1,27 @@
 const fs = require("fs").promises;
-const { resolve } = require('path');
+const { resolve } = require("path");
 
 const dirName = "./dist";
 // spelling out words that this file doesn't trigger any bad word filters.
 const parts = [
     ["b", "l", "a", "c", "k", "l", "i", "s", "t"],
-    ["w", "h", "i", "t", "e", "l", "i", "s", "t"]
+    ["w", "h", "i", "t", "e", "l", "i", "s", "t"],
 ];
-const wordsToUse = [
-    "dissallowlist",
-    "allowlist"
-];
+const wordsToUse = ["dissallowlist", "allowlist"];
 
-const wordsToReplace = parts.map(part => part.join(""));
+const wordsToReplace = parts.map((part) => part.join(""));
 const replacements = new Map();
 wordsToReplace.forEach((word, index) => replacements.set(word, wordsToUse[index]));
 const stringReplaceRegex = new RegExp(wordsToReplace.join("|"), "gi");
 
 async function getFiles(dir) {
     const dirents = await fs.readdir(dir, { withFileTypes: true });
-    const files = await Promise.all(dirents.map((dirent) => {
-      const res = resolve(dir, dirent.name);
-      return dirent.isDirectory() ? getFiles(res) : res;
-    }));
+    const files = await Promise.all(
+        dirents.map((dirent) => {
+            const res = resolve(dir, dirent.name);
+            return dirent.isDirectory() ? getFiles(res) : res;
+        })
+    );
     return Array.prototype.concat(...files);
 }
 
@@ -32,8 +31,7 @@ async function readAndReplace(fileName) {
         return replacements.get(matched);
     });
 
-    if (contents !== sanitized)
-        await fs.writeFile(fileName, sanitized);
+    if (contents !== sanitized) await fs.writeFile(fileName, sanitized);
 }
 
 async function sanitize() {
