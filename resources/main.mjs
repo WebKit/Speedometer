a608e983961de357c5410a88265543896cf369dd
Camillo Bruni <camillobruni@users.noreply.github.com>
2023-08-21 12:12:59 +0200

    Remove broken benchmark-runner.mjs file (#297)
    The file is currently unused and was webkit-CI specific.

--- resources/main.mjs
+++ resources/main.mjs
@@ -1,5 +1,4 @@
 import { BenchmarkRunner } from "./benchmark-runner.mjs";
-import "./benchmark-report.mjs";
 import * as Statistics from "./statistics.mjs";
 import { Suites } from "./tests.mjs";
 import { renderMetricView } from "./metric-ui.mjs";
