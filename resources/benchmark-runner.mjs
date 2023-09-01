34f44b40d206259c104c4fc513f131b3e097340a
Luis Fernando Pardo Sixtos <lpardosixtos@microsoft.com>
2023-09-01 10:15:12 -0700

    Delete extra line

--- resources/benchmark-runner.mjs
+++ resources/benchmark-runner.mjs
@@ -335,7 +335,6 @@ export class BenchmarkRunner {
         this._iterationCount = params.iterationCount;
         if (params.shuffleSeed !== "off")
             this._suiteOrderRandomNumberGenerator = seededHashRandomNumberGenerator(params.shuffleSeed);
-
     }
 
     async runMultipleIterations(iterationCount) {
