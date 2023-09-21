8fd080fb6706bdc7a94956720655795f4536dbbf
lpardosixtosMs <94007365+lpardosixtosMs@users.noreply.github.com>
2023-09-21 12:36:40 -0700

    [Complex-DOM] Fix generator node count (#308)
    * Fix count
    * Rewrite generator
    * Wrap the generator in a function
    * combine folderwrapper and treeitem
    * add class for type of item
    * Update dist folders
    
    ---------
    
    Co-authored-by: Julien Wajsberg <felash@gmail.com>
    Co-authored-by: Issack John <issack.john@outlook.com>

--- resources/todomvc/big-dom-generator/params.js
+++ resources/todomvc/big-dom-generator/params.js
@@ -1,9 +1,12 @@
 export const DEFAULT_SEED_FOR_RANDOM_NUMBER_GENERATOR = 4212021;
 
-// Constrains for the generated DOM tree.
+// Constraints for the generated tree-view side panel for the complex DOM shell
 export const MAX_GENERATED_DOM_DEPTH = 40;
 export const MAX_NUMBER_OF_CHILDREN = 16;
-export const PROBABILITY_OF_HAVING_CHILDREN = 0.7;
-export const TARGET_SIZE = 3000;
-
-export const MAX_VISIBLE_TREE_VIEW_ITEM_DEPTH = 15;
+export const PROBABILITY_OF_HAVING_CHILDREN = 0.3;
+// The target element count for the generated DOM tree.
+export const TARGET_SIZE = 6000;
+// The generator will ensure that the generated DOM tree has
+// at least MIN_NUMBER_OF_MAX_DEPTH_BRANCHES of depth MAX_GENERATED_DOM_DEPTH.
+export const MIN_NUMBER_OF_MAX_DEPTH_BRANCHES = 2;
+export const MAX_VISIBLE_TREE_VIEW_ITEM_DEPTH = 8;
---
+++ resources/todomvc/big-dom-generator/src/tree-generator.js
@@ -0,0 +1,89 @@
+import { LCG } from "random-seedable";
+import { DEFAULT_SEED_FOR_RANDOM_NUMBER_GENERATOR, MAX_GENERATED_DOM_DEPTH, MAX_NUMBER_OF_CHILDREN, PROBABILITY_OF_HAVING_CHILDREN, TARGET_SIZE, MIN_NUMBER_OF_MAX_DEPTH_BRANCHES } from "./../params";
+
+const random = new LCG(DEFAULT_SEED_FOR_RANDOM_NUMBER_GENERATOR);
+
+/**
+ * Generates the blueprint of the tree-view side panel for the complex DOM shell with expandable and non-expandable items.
+ * It starts with the minimum number of maximum-depth branches and randomly adds
+ * children to the nodes in a breadth-first manner.
+ * The weight parameters represent how many DOM nodes are generated for each type of node.
+ * @param {Object} options - The options object.
+ * @param {number} options.expandableItemWeight - The weight for the "expandableItem" node type. <li></li> with ChevronRight svg.
+ * @param {number} options.nonExpandableItemWeight - The weight for the "nonExpandableItem" node type. <li></li> TaskListIcon svg.
+ * @returns {Object} The generated tree structure with two possible values for the "type" property: "expandableItem" or "nonExpandableItem"
+ * Example structure:
+ * {
+ *    type: "expandableItem",
+ *    children: [
+ *      {
+ *         type: "expandableItem",
+ *         children: [
+ *           {
+ *             type: "nonExpandableItem",
+ *             children: []
+ *           }
+ *         ]
+ *      }
+ *    ]
+ * }
+ **/
+export const generateTreeHead = ({ expandableItemWeight, nonExpandableItemWeight }) => {
+    const treeHead = { type: "expandableItem", children: [] };
+    const nodeWeight = { expandableItem: expandableItemWeight, nonExpandableItem: nonExpandableItemWeight };
+
+    let totalNodes = expandableItemWeight;
+    for (let i = 0; i < MIN_NUMBER_OF_MAX_DEPTH_BRANCHES; i++) {
+        let currentDepth = 0;
+        let currentNode = treeHead;
+        while (currentDepth < MAX_GENERATED_DOM_DEPTH) {
+            let childType = "expandableItem";
+            currentNode.children.push({ type: childType, children: [] });
+            currentNode = currentNode.children[currentNode.children.length - 1];
+            currentDepth++;
+            totalNodes += nodeWeight[childType];
+        }
+    }
+
+    const treeNodes = [treeHead];
+    // Do an outer loop because there is a chance that the inner loop ends without
+    // reaching the target size.
+    while (totalNodes < TARGET_SIZE) {
+        let index = 0;
+        // All items start as closed and are marked open if the algorithm adds children.
+        while (index < treeNodes.length && totalNodes < TARGET_SIZE) {
+            let currentNode = treeNodes[index];
+            switch (currentNode.type) {
+                case "expandableItem":
+                    if (random.coin(PROBABILITY_OF_HAVING_CHILDREN) || currentNode.children.length) {
+                        const numberOfNewChildren = random.randRange(1, MAX_NUMBER_OF_CHILDREN - currentNode.children.length + 1);
+                        for (let i = 0; i < numberOfNewChildren && totalNodes < TARGET_SIZE; i++) {
+                            currentNode.children.push({ type: "nonExpandableItem", children: [] });
+                            totalNodes += nodeWeight["nonExpandableItem"];
+                        }
+                        random.shuffle(currentNode.children, true);
+                        treeNodes.push(...currentNode.children);
+                    }
+                    break;
+                case "nonExpandableItem":
+                    if (random.coin(PROBABILITY_OF_HAVING_CHILDREN)) {
+                        currentNode.type = "expandableItem";
+                        // randomly choose the child type between expandableItem and nonExpandableItem
+                        let childType = random.choice(["expandableItem", "nonExpandableItem"]);
+                        currentNode.children.push({ type: childType, children: [] });
+                        // We changed the node type so we need to update the totalNodes count.
+                        totalNodes = totalNodes - nodeWeight["nonExpandableItem"] + nodeWeight["expandableItem"];
+                        // We added a child so we need to update the totalNodes count.
+                        totalNodes += nodeWeight[childType];
+                        treeNodes.push(currentNode.children[0]);
+                    }
+                    break;
+                default:
+                    throw new Error(`Unknown node type: ${currentNode.type}`);
+            }
+            index++;
+        }
+    }
+
+    return treeHead;
+};
