cbab6de0ccdb7cbd9613272e6e0f1fecca585507
Issack John <issack.john@outlook.com>
2023-09-26 12:39:27 -0700

    Follow up for complex DOM versions. (#41)
    * add comments
    
    * check that JSDOM is present
    
    * Check prereqs in the build script and handle them
    
    * group requires
    
    * change the wording
    
    * change some more format
    
    * fix up rebase.
    
    * add the prerequisite steps to the util function
    
    * add code comment.
    
    * code clean up
    
    * follow up comment.
    
    * use npm ci instead
    
    * inline some constants
    
    * spli up second argument.
    
    * remove dev dependency

--- resources/todomvc/architecture-examples/angular-complex/scripts/build.js
+++ resources/todomvc/architecture-examples/angular-complex/scripts/build.js
@@ -1,17 +1,18 @@
+/**
+ * Builds the TodoMVC Angular Complex DOM.
+ */
 const path = require("path");
 const { buildComplex } = require("big-dom-generator/utils/buildComplex");
 
-const SOURCE_DIRECTORY = "node_modules/todomvc-angular/dist/";
-const TITLE = "TodoMVC: Angular Complex DOM";
-const FILES_TO_MOVE = ["node_modules/big-dom-generator/dist/big-dom-generator.css", "node_modules/big-dom-generator/dist/logo.png"];
-
 const options = {
     callerDirectory: path.resolve(__dirname),
-    sourceDirectory: path.join("..", SOURCE_DIRECTORY),
-    title: TITLE,
-    filesToMove: FILES_TO_MOVE,
+    sourceDirectory: path.join("..", "node_modules", "todomvc-angular", "dist"),
+    title: "TodoMVC: Angular Complex DOM",
+    filesToMove: ["node_modules/big-dom-generator/dist/big-dom-generator.css", "node_modules/big-dom-generator/dist/logo.png"],
     cssFilePath: path.resolve(__dirname, "..", "node_modules", "big-dom-generator", "utils", "app.css"),
     cssFileNamePattern: /^styles.*\.css$/,
+    standaloneDirectory: path.resolve(__dirname, "..", "..", "angular"),
+    complexDirectory: path.resolve(__dirname, ".."),
 };
 
 buildComplex(options);
