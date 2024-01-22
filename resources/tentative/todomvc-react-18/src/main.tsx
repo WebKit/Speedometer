8aa32b08b01213eab3d6b8e948449e25bad1afef
Julien Wajsberg <felash@gmail.com>
2024-01-22 16:22:05 +0100

    Add Material UI

--- resources/tentative/todomvc-react-18/src/main.tsx
+++ resources/tentative/todomvc-react-18/src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App.tsx";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
+        <CssBaseline />
         <App />
     </React.StrictMode>
 );
