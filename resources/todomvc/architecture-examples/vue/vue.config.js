58e2e4e92bd7192fc749f78534fd2098598bdafa
Thorsten Kober <tkober@gmail.com>
2024-03-01 20:19:10 -0500

    Revert "Todomvc-vue: source-map creation (#386)" (#388)
    This reverts commit 7dc43d27a51ed6a32e062485a8590e68a8abd962.

--- resources/todomvc/architecture-examples/vue/vue.config.js
+++ resources/todomvc/architecture-examples/vue/vue.config.js
@@ -1,11 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
-const webpack = require("webpack");
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: "",
-    configureWebpack: {
-        plugins: [new webpack.EvalSourceMapDevToolPlugin({})],
-    },
     terser: {
         minify: "terser",
         terserOptions: {
