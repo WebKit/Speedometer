7dc43d27a51ed6a32e062485a8590e68a8abd962
Thorsten Kober <tkober@gmail.com>
2024-03-01 19:19:56 -0500

    Todomvc-vue: source-map creation (#386)

--- resources/todomvc/architecture-examples/vue/vue.config.js
+++ resources/todomvc/architecture-examples/vue/vue.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: "",
+    configureWebpack: {
+        plugins: [new webpack.EvalSourceMapDevToolPlugin({})],
+    },
     terser: {
         minify: "terser",
         terserOptions: {
