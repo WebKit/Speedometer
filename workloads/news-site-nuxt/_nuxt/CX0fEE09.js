77d1082867edb1c4e5f072613ff4a3f7b940f478
Thorsten Kober <tkober@gmail.com>
2024-05-20 14:01:08 -0400

    use speedometer-workloads

---
+++ workloads/news-site-nuxt/_nuxt/CX0fEE09.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./h_8ojW8j.js";import{u as s,g as t,o as c}from"./CGxvoaxh.js";const p={__name:"us",setup(a){return s({script:[{src:"./benchmark-connector.min.js",type:"module",tagPosition:"bodyClose"}]}),(n,r)=>{const o=e;return c(),t(o,{id:"us"})}}};export{p as default};
+//# sourceMappingURL=CX0fEE09.js.map
