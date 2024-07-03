e44df00524a9ee65c6ed4d452d675df7918aee71
Thorsten Kober <tkober@gmail.com>
2024-07-03 13:49:04 -0400

    add emoji test

--- resources/tests.mjs
+++ resources/tests.mjs
@@ -218,7 +218,7 @@ Suites.push({
         new BenchmarkTestStep(`Adding${numberOfItemsToAdd}Items`, (page) => {
             const input = page.querySelector(".new-todo-input", ["todo-app", "todo-topbar"]);
             for (let i = 0; i < numberOfItemsToAdd; i++) {
-                input.setValue(getTodoText(defaultLanguage, i));
+                input.setValue(getTodoText("emoji", i));
                 input.dispatchEvent("input");
                 input.enter("keyup");
             }
