6db97351e9182b9b165ebacc87682a1cbda97c5d
Lingyun Cai <lingyun.cai@intel.com>
2024-08-22 11:18:04 +0800

    Avoid key update for every re-render of ArticleContent with the same content
    Use `useMemo()` to avoid uuidv4() call on the same content, which introduced
    unnecessary re-renders of the same <li> element.

--- resources/newssite/news-next/src/components/article/article-content.jsx
+++ resources/newssite/news-next/src/components/article/article-content.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { v4 as uuidv4 } from "uuid";
 import classNames from "classnames";
 
@@ -8,6 +9,15 @@ import styles from "news-site-css/dist/article.module.css";
 import layoutStyles from "news-site-css/dist/layout.module.css";
 
 export default function ArticleContent({ type, content, display }) {
+    function createContentWithId(type, content) {
+        return type !== "text" ? content.map((item) => ({ ...item, id: uuidv4() })) : content;
+    }
+
+    const contentWithId = useMemo(
+        () => createContentWithId(type, content),
+        [type, content]
+    );
+
     if (type === "text") {
         return (
             <div className={styles["article-content"]}>
@@ -20,8 +30,8 @@ export default function ArticleContent({ type, content, display }) {
         return (
             <div className={styles["article-content"]}>
                 <ul className={classNames(styles["article-list"], styles.vertical, { [styles[display]]: display })}>
-                    {content.map((item) =>
-                        <li key={uuidv4()} className={styles["article-list-item"]}>
+                    {contentWithId.map((item) =>
+                        <li key={item.id} className={styles["article-list-item"]}>
                             {item.url && !item.title
                                 ? <a href={item.url}>
                                     <ArticleText text={item.content} />
@@ -39,8 +49,8 @@ export default function ArticleContent({ type, content, display }) {
         return (
             <div className={styles["article-list-content"]}>
                 <ul className={classNames(styles["article-list"], styles.vertical)}>
-                    {content.map((item) =>
-                        <li key={uuidv4()} className={styles["article-list-item"]}>
+                    {contentWithId.map((item) =>
+                        <li key={item.id} className={styles["article-list-item"]}>
                             <ArticleText textClass={classNames(styles["article-title"], "truncate-multiline", "truncate-multiline-3")} text={item.title} type="h3" />
                             {item.url && !item.title
                                 ? <a href={item.url}>
@@ -58,8 +68,8 @@ export default function ArticleContent({ type, content, display }) {
     if (type === "excerpt") {
         return (
             <ul className={classNames(styles["article-list"], styles.horizontal)}>
-                {content.map((item) =>
-                    <li key={uuidv4()} className={styles["article-list-item"]}>
+                {contentWithId.map((item) =>
+                    <li key={item.id} className={styles["article-list-item"]}>
                         <ArticleImage imageClass={styles["article-hero"]} image={item.image} />
                         <div className={styles["article-content"]}>
                             <ArticleText textClass="truncate-multiline truncate-multiline-3" text={item.text} type="div" />
@@ -73,8 +83,8 @@ export default function ArticleContent({ type, content, display }) {
     if (type === "grid") {
         return (
             <div className={classNames(layoutStyles["grid-container"], { [layoutStyles[display]]: display })}>
-                {content.map((item) =>
-                    <div key={uuidv4()} className={layoutStyles["grid-item"]}>
+                {contentWithId.map((item) =>
+                    <div key={item.id} className={layoutStyles["grid-item"]}>
                         <ArticleImage imageClass={styles["article-image-container"]} image={item.image} meta={item.meta} />
 
                         {item.url
@@ -92,8 +102,8 @@ export default function ArticleContent({ type, content, display }) {
     if (type === "preview") {
         return (
             <ul className={classNames(styles["article-list"], styles.vertical)}>
-                {content.map((item) =>
-                    <li key={uuidv4()} className={styles["article-list-item"]}>
+                {contentWithId.map((item) =>
+                    <li key={item.id} className={styles["article-list-item"]}>
                         <ArticleImage imageClass={styles["article-image-container"]} image={item.image} />
                         <ArticleText textClass={classNames(styles["article-title"], "truncate-multiline", "truncate-multiline-3")} text={item.title} type="h3" />
                     </li>
