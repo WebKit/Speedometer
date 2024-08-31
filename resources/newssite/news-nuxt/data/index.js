ca420f43240171f100c79871d35eb13562f4b831
Thorsten Kober <tkober@gmail.com>
2024-08-31 08:14:07 -0400

    update data

--- resources/newssite/news-nuxt/data/index.js
+++ resources/newssite/news-nuxt/data/index.js
@@ -1,22 +1,22 @@
-import { content as contentEn } from "~/data/en/content";
-import { content as contentJp } from "~/data/jp/content";
-import { content as contentAr } from "~/data/ar/content";
+import { content as contentEn } from "@/data/en/content";
+import { content as contentJp } from "@/data/jp/content";
+import { content as contentAr } from "@/data/ar/content";
 
-import { settings as settingsEn } from "~/data/en/dialog";
-import { settings as settingsJp } from "~/data/jp/dialog";
-import { settings as settingsAr } from "~/data/ar/dialog";
+import { settings as settingsEn } from "@/data/en/dialog";
+import { settings as settingsJp } from "@/data/jp/dialog";
+import { settings as settingsAr } from "@/data/ar/dialog";
 
-import { footer as footerEn } from "~/data/en/footer";
-import { footer as footerJp } from "~/data/jp/footer";
-import { footer as footerAr } from "~/data/ar/footer";
+import { footer as footerEn } from "@/data/en/footer";
+import { footer as footerJp } from "@/data/jp/footer";
+import { footer as footerAr } from "@/data/ar/footer";
 
-import * as buttonsEn from "~/data/en/buttons";
-import * as buttonsJp from "~/data/jp/buttons";
-import * as buttonsAr from "~/data/ar/buttons";
+import * as buttonsEn from "@/data/en/buttons";
+import * as buttonsJp from "@/data/jp/buttons";
+import * as buttonsAr from "@/data/ar/buttons";
 
-import * as linksEn from "~/data/en/links";
-import * as linksJp from "~/data/jp/links";
-import * as linksAr from "~/data/ar/links";
+import * as linksEn from "@/data/en/links";
+import * as linksJp from "@/data/jp/links";
+import * as linksAr from "@/data/ar/links";
 
 export const dataSource = {
     en: {
