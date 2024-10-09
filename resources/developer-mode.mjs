d68f82b2f6acb7665e484beb0b29177379538d24
Camillo Bruni <cbruni@chromium.org>
2024-10-09 10:21:46 -0400

    pre-format

--- resources/developer-mode.mjs
+++ resources/developer-mode.mjs
@@ -240,13 +240,10 @@ function updateURL() {
     // to comma separate only the selected
     const selectedSuites = Suites.filter((suite) => !suite.disabled);
 
-    if (!selectedSuites.length) {
-        url.searchParams.delete("tags");
-        url.searchParams.delete("suites");
-        url.searchParams.delete("suite");
-    } else {
-        url.searchParams.delete("tags");
-        url.searchParams.delete("suite");
+    url.searchParams.delete("tags");
+    url.searchParams.delete("suites");
+    url.searchParams.delete("suite");
+    if (selectedSuites.length) {
         // Try finding common tags that would result in the current suite selection.
         let commonTags = new Set(selectedSuites[0].tags);
         for (const suite of Suites) {
@@ -257,13 +254,10 @@ function updateURL() {
         }
         if (selectedSuites.length > 1 && commonTags.size) {
             const tags = [...commonTags][0];
-            if (tags === "default")
-                url.searchParams.delete("tags");
-            else
+            if (tags !== "default")
                 url.searchParams.set("tags", tags);
             url.searchParams.delete("suites");
         } else {
-            url.searchParams.delete("tags");
             url.searchParams.set("suites", selectedSuites.map((suite) => suite.name).join(","));
         }
     }
