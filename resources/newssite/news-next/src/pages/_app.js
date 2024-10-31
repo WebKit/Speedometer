14f1f6d5235c492ab6b8db12ac6bf99e675f520d
Thorsten Kober <tkober@gmail.com>
2024-10-31 14:56:28 -0400

    undo newssite changes

--- resources/newssite/news-next/src/pages/_app.js
+++ resources/newssite/news-next/src/pages/_app.js
@@ -5,10 +5,22 @@ import "news-site-css/dist/icons.css";
 import "news-site-css/dist/text.css";
 
 import { useEffect, useState } from "react";
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
     const [render, setRender] = useState(false);
     useEffect(() => setRender(true), []);
-    return render ? <Component {...pageProps} /> : null;
+    return render
+        ? <>
+            <Script id="raf-mock">{`// This hack allows to capture the work normally happening in a rAF. We
+// may be able to remove it if the runner improves.
+window.requestAnimationFrame = (cb) => window.setTimeout(cb, 0);
+window.cancelAnimationFrame = window.clearTimeout;
+// Disable requestIdleCallback until WebKit / Safari supports it.
+window.requestIdleCallback = undefined;
+window.cancelIdleCallback = undefined;`}</Script>
+            <Component {...pageProps} />
+        </>
+        : null;
 }
 export default App;
