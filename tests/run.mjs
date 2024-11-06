94f0d816864f4c7e7f668011c98931417d157a3c
Thorsten Kober <tkober@gmail.com>
2024-11-06 08:24:54 -0500

    Test Runner cleanup - for our internal tests (#446)

--- tests/run.mjs
+++ tests/run.mjs
@@ -1,6 +1,5 @@
 #! /usr/bin/env node
 /* eslint-disable-next-line  no-unused-vars */
-import { argv } from "node:process";
 import serve from "./server.mjs";
 import { Builder, Capabilities } from "selenium-webdriver";
 import commandLineArgs from "command-line-args";
@@ -71,22 +70,56 @@ const server = serve(PORT);
 
 let driver;
 
+function printTree(node) {
+    console.log(node.title);
+
+    for (const test of node.tests) {
+        console.group();
+        if (test.state === "passed") {
+            console.log("\x1b[32m✓", `\x1b[0m${test.title}`);
+        } else {
+            console.log("\x1b[31m✖", `\x1b[0m${test.title}`);
+            console.group();
+            console.log(`\x1b[31m${test.error.name}: ${test.error.message}`);
+            console.groupEnd();
+        }
+        console.groupEnd();
+    }
+
+    for (const suite of node.suites) {
+        console.group();
+        printTree(suite);
+        console.groupEnd();
+    }
+}
+
 async function test() {
     driver = await new Builder().withCapabilities(capabilities).build();
 
     try {
         await driver.get(`http://localhost:${PORT}/tests/index.html`);
-        console.log("Waiting for tests to finish");
-        const stats = await driver.executeAsyncScript(function (callback) {
-            window.addEventListener("complete", () => callback(window.mochaResults.stats), { once: true });
+        const result = await driver.executeAsyncScript(function (callback) {
+            window.addEventListener(
+                "test-complete",
+                () =>
+                    callback({
+                        stats: window.mochaResults.stats,
+                        suite: window.suite,
+                    }),
+                { once: true }
+            );
+            const event = new Event("start-test");
+            window.dispatchEvent(event);
         });
-        console.log("stats", stats);
-        console.log("Checking for passed tests");
-        assert(stats.passes > 0);
-        console.log("Checking for failed tests");
-        assert(stats.failures === 0);
+
+        printTree(result.suite);
+
+        console.log("\nChecking for passed tests...");
+        assert(result.stats.passes > 0);
+        console.log("Checking for failed tests...");
+        assert(result.stats.failures === 0);
     } finally {
-        console.log("Tests complete");
+        console.log("\nTests complete!");
         driver.quit();
         server.close();
     }
