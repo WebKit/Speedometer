d08606dbab495411e8332aa6c5e80ee744ad3c04
Thorsten Kober <tkober@gmail.com>
2024-11-08 11:43:52 -0500

    remove test test

--- resources/tests.mjs
+++ resources/tests.mjs
@@ -938,55 +938,6 @@ Suites.push({
 });
 
 Suites.push({
-    name: "NewsSite-Next-Delayed",
-    url: "resources/newssite/news-next/dist/index.html#/home",
-    tags: ["experimental", "newssite", "language"],
-    type: "async",
-    disabled: true,
-    async prepare(page) {
-        await page.waitForElement("#navbar-dropdown-toggle");
-    },
-    tests: [
-        new BenchmarkTestStep("NavigateToUS", async (page) => {
-            for (let i = 0; i < 25; i++) {
-                page.querySelector("#navbar-dropdown-toggle").click();
-                page.layout();
-                page.querySelector("#navbar-dropdown-toggle").click();
-                page.layout();
-            }
-            page.querySelector("#navbar-navlist-us-link").click();
-            page.layout();
-
-            await ((ms) => new Promise((resolve) => setTimeout(() => resolve(), ms)))(500);
-        }),
-        new BenchmarkTestStep("NavigateToWorld", async (page) => {
-            for (let i = 0; i < 25; i++) {
-                page.querySelector("#navbar-dropdown-toggle").click();
-                page.layout();
-                page.querySelector("#navbar-dropdown-toggle").click();
-                page.layout();
-            }
-            page.querySelector("#navbar-navlist-world-link").click();
-            page.layout();
-
-            await ((ms) => new Promise((resolve) => setTimeout(() => resolve(), ms)))(500);
-        }),
-        new BenchmarkTestStep("NavigateToPolitics", async (page) => {
-            for (let i = 0; i < 25; i++) {
-                page.querySelector("#navbar-dropdown-toggle").click();
-                page.layout();
-                page.querySelector("#navbar-dropdown-toggle").click();
-                page.layout();
-            }
-            page.querySelector("#navbar-navlist-politics-link").click();
-            page.layout();
-
-            await ((ms) => new Promise((resolve) => setTimeout(() => resolve(), ms)))(500);
-        }),
-    ],
-});
-
-Suites.push({
     name: "NewsSite-Nuxt",
     url: "resources/newssite/news-nuxt/dist/index.html",
     tags: ["newssite"],
