b8ec788188bd16ca55e5140a02ea9009df8e3c90
Thorsten Kober <tkober@gmail.com>
2024-11-13 14:31:45 -0500

    use test-invoker

---
+++ resources/newssite/news-next/scripts/prepare.js
@@ -0,0 +1,55 @@
+const fs = require("fs").promises;
+const { dirname } = require("path");
+/**
+ * deleteFile
+ *
+ * Deletes a file with a src input.
+ *
+ * @param {string} src Path to the file to delete.
+ */
+async function deleteFile(src) {
+    try {
+        await fs.unlink(src);
+        console.log(`File ${src} has been deleted.`);
+    } catch (err) {
+        console.error("No previous file exists, no need to delete!");
+    }
+}
+
+/**
+ * copyFile
+ *
+ * Copies a file from a source to a destination.
+ *
+ * @param {string} src Source file.
+ * @param {string} dest Destination file.
+ */
+async function copyFile(src, dest) {
+    await fs.mkdir(dirname(dest), { recursive: true });
+    await fs.copyFile(src, dest);
+}
+
+/**
+ * prepare
+ *
+ * Function that copies the workload-testing-utils.min file to a new location.
+ * An optional 'HOST' environment variable can be used to assign a custom host directory.
+ *
+ */
+async function prepare() {
+    const moduleDirectory
+    = process.env.MODULE ?? "../../";
+    const hostDirectory = process.env.HOST ?? "public/";
+    const filesString = process.env.FILES ?? "test-invoker.mjs";
+
+    const files = filesString.split(",");
+
+    for (const file of files) {
+        await deleteFile(`${hostDirectory}${file}`);
+        await copyFile(`${moduleDirectory}${file}`, `${hostDirectory}${file}`);
+    }
+
+    console.log("Done with preparation!");
+}
+
+prepare();
