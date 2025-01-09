d6b5ffea959ad31e231c203d7446bf8b39e987ce
Camillo Bruni <camillobruni@users.noreply.github.com>
2025-01-09 17:08:47 +0100

    Display non-standard params on summary page (#469)
    Make sure we better highlight non default params since they affect the default score.
    
    - Display non-default params table in the summary view
    - Simplify defaultParams creation code a bit
    - Rebuild news-next since params.mjs changed

---
+++ resources/newssite/news-next/dist/_next/static/slmX88Sy3MO0SLK8cVHnz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/69253d842fd3fbfd.css","static/chunks/553-29fd8c03a09c7b37.js","static/chunks/pages/index-7052462c4e106c39.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
