a3b0e71d97a15cb6da2ca9ac60d7a8a0fe824824
Thorsten Kober <tkober@gmail.com>
2025-01-23 12:19:03 -0500

    News site Next: run build command (#474)

--- resources/newssite/news-next/dist/_next/static/IbZpY6lpxNCTHGQFLD2XQ/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:resources/newssite/news-next/dist/_next/static/IbZpY6lpxNCTHGQFLD2XQ/_buildManifest.js
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-fd706aeabb7828e3.js","static/css/2cf5163b53bb0adb.css","static/chunks/pages/index-aded717294ad42d4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
-========
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-fd706aeabb7828e3.js","static/css/2cf5163b53bb0adb.css","static/chunks/pages/index-5268ea812327eb5e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
->>>>>>>> main:resources/newssite/news-next/dist/_next/static/dKI4-ZnFPRMFEw-AlcuLr/_buildManifest.js
