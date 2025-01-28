a9f7e3c738aa70630e275d16ecc20dbd8de09aa0
Camillo Bruni <cbruni@chromium.org>
2025-01-28 13:53:06 +0100

    address comments - await server startup status - enable logging

--- tests/server.mjs
+++ tests/server.mjs
@@ -5,7 +5,6 @@ import commandLineArgs from "command-line-args";
 import esMain from "es-main";
 import LocalWebServer from "local-web-server";
 
-
 const ROOT_DIR = path.join(process.cwd(), "./");
 
 export default async function serve(port) {
@@ -16,20 +15,34 @@ export default async function serve(port) {
         directory: ROOT_DIR,
         corsOpenerPolicy: "same-origin",
         corsEmbedderPolicy: "require-corp",
+        logFormat: "dev",
     });
-    console.log(`Server started on http://localhost:${port}`);
+    await verifyStartup(ws, port);
+
     process.on("exit", () => ws.server.close());
+
     return {
         close() {
             ws.server.close();
-        }
+        },
     };
 }
 
+async function verifyStartup(ws, port) {
+    await new Promise((resolve, reject) => {
+        ws.server.on("listening", () => {
+            console.log(`Server started on http://localhost:${port}`);
+            resolve();
+        });
+        ws.server.on("error", (e) => {
+            console.error("Error while starting the server", e);
+            reject(e);
+        });
+    });
+}
+
 function main() {
-    const optionDefinitions = [
-        { name: "port", type: Number, defaultValue: 8080, description: "Set the test-server port, The default value is 8080." },
-    ];
+    const optionDefinitions = [{ name: "port", type: Number, defaultValue: 8080, description: "Set the test-server port, The default value is 8080." }];
     const options = commandLineArgs(optionDefinitions);
     serve(options.port);
 }
