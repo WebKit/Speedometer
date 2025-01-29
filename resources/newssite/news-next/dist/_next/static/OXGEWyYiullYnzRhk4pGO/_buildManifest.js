e8d6fd5412efd7d4ce1d17711fcf362f6888e948
Thorsten Kober <tkober@gmail.com>
2025-01-29 15:29:02 -0500

    rebuild after merge

--- resources/newssite/news-next/dist/_next/static/OXGEWyYiullYnzRhk4pGO/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:resources/newssite/news-next/dist/_next/static/OXGEWyYiullYnzRhk4pGO/_buildManifest.js
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-fd706aeabb7828e3.js","static/css/2cf5163b53bb0adb.css","static/chunks/pages/index-e11ab37c31670d4a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
-========
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/743-fd706aeabb7828e3.js","static/css/2cf5163b53bb0adb.css","static/chunks/pages/index-d614e8004ed1a15b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
->>>>>>>> release/3.1:resources/newssite/news-next/dist/_next/static/Jwc4rJ4Pv1N1tzU_L-d5Q/_buildManifest.js
