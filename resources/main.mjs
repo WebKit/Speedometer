2e3109fa8a1138d477acbbfbf15b99d5d05ea582
Lingyun Cai <lingyun.cai@intel.com>
2025-03-12 00:15:33 +0800

    Ensure the benchmark version in the index page title (#503)

--- resources/main.mjs
+++ resources/main.mjs
@@ -389,7 +389,7 @@ class MainBenchmarkClient {
     _updateDocumentTitle(hash) {
         const maybeSection = document.querySelector(hash);
         const sectionTitle = maybeSection?.getAttribute("data-title") ?? "";
-        document.title = `Speedometer 3 ${sectionTitle}`.trimEnd();
+        document.title = `Speedometer 3.1 ${sectionTitle}`.trimEnd();
     }
 
     _removeLocationHash() {
