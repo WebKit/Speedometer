efd05f5ece278b55dd1d3dc801eee1d7c3cc2165
Camillo Bruni <cbruni@chromium.org>
2025-10-07 22:42:29 +0200

    partial revert

--- resources/newssite/news-next/src/workload-test.mjs
+++ resources/newssite/news-next/src/workload-test.mjs
@@ -1,48 +1,42 @@
-import { BenchmarkStep, AsyncBenchmarkStep, AsyncBenchmarkSuite } from "speedometer-utils/benchmark.mjs";
+import { BenchmarkStep, BenchmarkSuite } from "speedometer-utils/benchmark.mjs";
 import { forceLayout, getElement } from "speedometer-utils/helpers.mjs";
 
 const suites = {
-    default: new AsyncBenchmarkSuite(
-        "default",
-        [
-            new AsyncBenchmarkStep("Navigate-to-US-page", async () => {
-                for (let i = 0; i < 25; i++) {
-                    getElement("#navbar-dropdown-toggle").click();
-                    forceLayout();
-                    getElement("#navbar-dropdown-toggle").click();
-                    forceLayout();
-                }
-                await new Promise((r) => setTimeout(r, 1000));
-
-                getElement("#navbar-navlist-us-link").click();
+    default: new BenchmarkSuite("default", [
+        new BenchmarkStep("Navigate-to-US-page", () => {
+            for (let i = 0; i < 25; i++) {
+                getElement("#navbar-dropdown-toggle").click();
                 forceLayout();
-            }),
-
-            new BenchmarkStep("Navigate-to-World-page", () => {
-                for (let i = 0; i < 25; i++) {
-                    getElement("#navbar-dropdown-toggle").click();
-                    forceLayout();
-                    getElement("#navbar-dropdown-toggle").click();
-                    forceLayout();
-                }
-
-                getElement("#navbar-navlist-world-link").click();
+                getElement("#navbar-dropdown-toggle").click();
                 forceLayout();
-            }),
-            new BenchmarkStep("Navigate-to-Politics-page", () => {
-                for (let i = 0; i < 25; i++) {
-                    getElement("#navbar-dropdown-toggle").click();
-                    forceLayout();
-                    getElement("#navbar-dropdown-toggle").click();
-                    forceLayout();
-                }
+            }
 
-                getElement("#navbar-navlist-politics-link").click();
+            getElement("#navbar-navlist-us-link").click();
+            forceLayout();
+        }),
+        new BenchmarkStep("Navigate-to-World-page", () => {
+            for (let i = 0; i < 25; i++) {
+                getElement("#navbar-dropdown-toggle").click();
                 forceLayout();
-            }),
-        ],
-        true
-    ),
+                getElement("#navbar-dropdown-toggle").click();
+                forceLayout();
+            }
+
+            getElement("#navbar-navlist-world-link").click();
+            forceLayout();
+        }),
+        new BenchmarkStep("Navigate-to-Politics-page", () => {
+            for (let i = 0; i < 25; i++) {
+                getElement("#navbar-dropdown-toggle").click();
+                forceLayout();
+                getElement("#navbar-dropdown-toggle").click();
+                forceLayout();
+            }
+
+            getElement("#navbar-navlist-politics-link").click();
+            forceLayout();
+        }),
+    ]),
 };
 
 export default suites;
