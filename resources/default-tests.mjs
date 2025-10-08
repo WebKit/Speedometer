db2497918b3b92e78779839871e3a49e0ee1f006
Luis Pardo <lpardosixtos@microsoft.com>
2025-10-08 17:27:02 +0000

    Update test files

--- resources/default-tests.mjs
+++ resources/default-tests.mjs
@@ -5,6 +5,184 @@ import { freezeSuites } from "../resources/suites-helper.mjs";
 
 export const DefaultSuites = freezeSuites([
     {
+        name: "TodoMVC-WebComponents-IndexedDB",
+        url: "experimental/javascript-wc-indexeddb/dist/index.html",
+        tags: ["todomvc", "webcomponents", "experimental"],
+        type: "async",
+        async prepare(page) {
+            await page.waitForElement("todo-app");
+            await page.waitForElement(".indexeddb-ready");
+            page.setGlobalVariable(
+                "addPromise",
+                new Promise((resolve) => {
+                    page.addEventListener("indexeddb-add-completed", () => {
+                        resolve();
+                    });
+                })
+            );
+            page.setGlobalVariable(
+                "completePromise",
+                new Promise((resolve) => {
+                    page.addEventListener("indexeddb-toggle-completed", () => {
+                        resolve();
+                    });
+                })
+            );
+            page.setGlobalVariable(
+                "removePromise",
+                new Promise((resolve) => {
+                    page.addEventListener("indexeddb-remove-completed", () => {
+                        resolve();
+                    });
+                })
+            );
+        },
+        tests: [
+            new BenchmarkTestStep(`Adding${numberOfItemsToAdd}Items`, async (page) => {
+                const input = page.querySelector(".new-todo-input", ["todo-app", "todo-topbar"]);
+                for (let i = 0; i < numberOfItemsToAdd; i++) {
+                    input.setValue(getTodoText(defaultLanguage, i));
+                    input.dispatchEvent("input");
+                    input.enter("keyup");
+                }
+            }),
+            new BenchmarkTestStep(
+                "FinishAddingItemsToDB",
+                async (page) => {
+                    await page.getGlobalVariable("addPromise");
+                },
+                true
+            ),
+            new BenchmarkTestStep("CompletingAllItems", async (page) => {
+                const numberOfItemsPerIteration = 10;
+                const numberOfIterations = 10;
+                page.setGlobalVariable("numberOfItemsToAdd", numberOfItemsToAdd);
+                for (let j = 0; j < numberOfIterations; j++) {
+                    const items = page.querySelectorAll("todo-item", ["todo-app", "todo-list"]);
+                    for (let i = 0; i < numberOfItemsPerIteration; i++) {
+                        const item = items[i].querySelectorInShadowRoot(".toggle-todo-input");
+                        item.click();
+                    }
+                    // // Let the layout update???
+                    // // Give a change to the pending indexedDB operations to complete in main thread.
+                    // await new Promise((resolve) => {
+                    //     setTimeout(() => {resolve();}, 0);
+                    // });
+                    if (j < 9) {
+                        const nextPageButton = page.querySelector(".next-page-button", ["todo-app", "todo-bottombar"]);
+                        nextPageButton.click();
+                    }
+                }
+            }),
+            new BenchmarkTestStep(
+                "FinishModifyingItemsInDB",
+                async (page) => {
+                    await page.getGlobalVariable("completePromise");
+                },
+                true
+            ),
+            new BenchmarkTestStep("DeletingAllItems", async (page) => {
+                const numberOfItemsPerIteration = 10;
+                const numberOfIterations = 10;
+                page.setGlobalVariable("numberOfItemsToAdd", numberOfItemsToAdd);
+                function iterationFinishedListener() {
+                    iterationFinishedListener.promiseResolve();
+                }
+                page.addEventListener("previous-page-loaded", iterationFinishedListener);
+                for (let j = 0; j < numberOfIterations; j++) {
+                    const iterationFinishedPromise = new Promise((resolve) => {
+                        iterationFinishedListener.promiseResolve = resolve;
+                    });
+                    const items = page.querySelectorAll("todo-item", ["todo-app", "todo-list"]);
+                    for (let i = numberOfItemsPerIteration - 1; i >= 0; i--) {
+                        const item = items[i].querySelectorInShadowRoot(".remove-todo-button");
+                        item.click();
+                    }
+                    // Let the layout update???
+                    // Give a change to the pending indexedDB operations to complete in main thread.
+                    await new Promise((resolve) => {
+                        setTimeout(() => {
+                            resolve();
+                        }, 0);
+                    });
+                    if (j < 9) {
+                        const previousPageButton = page.querySelector(".previous-page-button", ["todo-app", "todo-bottombar"]);
+                        previousPageButton.click();
+                        await iterationFinishedPromise;
+                    }
+                }
+            }),
+            new BenchmarkTestStep(
+                "FinishDeletingItemsFromDB",
+                async (page) => {
+                    await page.getGlobalVariable("removePromise");
+                },
+                true
+            ),
+        ],
+    },
+    {
+        name: "TodoMVC-LocalStorage",
+        url: "experimental/todomvc-localstorage/dist/index.html",
+        tags: ["todomvc"],
+        async prepare(page) {
+            (await page.waitForElement(".new-todo")).focus();
+            page.getLocalStorage().getItem("javascript-es5");
+        },
+        tests: [
+            new BenchmarkTestStep(`Adding${numberOfItemsToAdd}Items`, (page) => {
+                const newTodo = page.querySelector(".new-todo");
+                for (let i = 0; i < numberOfItemsToAdd; i++) {
+                    newTodo.setValue(getTodoText("ja", i));
+                    newTodo.dispatchEvent("change");
+                    newTodo.enter("keypress");
+                }
+            }),
+            new BenchmarkTestStep("CompletingAllItems", (page) => {
+                const checkboxes = page.querySelectorAll(".toggle");
+                for (let i = 0; i < numberOfItemsToAdd; i++)
+                    checkboxes[i].click();
+            }),
+            new BenchmarkTestStep("DeletingAllItems", (page) => {
+                const deleteButtons = page.querySelectorAll(".destroy");
+                for (let i = numberOfItemsToAdd - 1; i >= 0; i--)
+                    deleteButtons[i].click();
+            }),
+        ],
+    },
+    {
+        name: "TodoMVC-Emoji",
+        url: "resources/todomvc/vanilla-examples/javascript-web-components/dist/index.html",
+        tags: ["todomvc", "experimental"],
+        async prepare(page) {
+            await page.waitForElement("todo-app");
+        },
+        tests: [
+            new BenchmarkTestStep(`Adding${numberOfItemsToAdd}Items`, (page) => {
+                const input = page.querySelector(".new-todo-input", ["todo-app", "todo-topbar"]);
+                for (let i = 0; i < numberOfItemsToAdd; i++) {
+                    input.setValue(getTodoText("emoji", i));
+                    input.dispatchEvent("input");
+                    input.enter("keyup");
+                }
+            }),
+            new BenchmarkTestStep("CompletingAllItems", (page) => {
+                const items = page.querySelectorAll("todo-item", ["todo-app", "todo-list"]);
+                for (let i = 0; i < numberOfItemsToAdd; i++) {
+                    const item = items[i].querySelectorInShadowRoot(".toggle-todo-input");
+                    item.click();
+                }
+            }),
+            new BenchmarkTestStep("DeletingAllItems", (page) => {
+                const items = page.querySelectorAll("todo-item", ["todo-app", "todo-list"]);
+                for (let i = numberOfItemsToAdd - 1; i >= 0; i--) {
+                    const item = items[i].querySelectorInShadowRoot(".remove-todo-button");
+                    item.click();
+                }
+            }),
+        ],
+    },
+    {
         name: "TodoMVC-JavaScript-ES5",
         url: "resources/todomvc/vanilla-examples/javascript-es5/dist/index.html",
         tags: ["default", "todomvc"],
