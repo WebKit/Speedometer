7ef17561105555a7493702418bb699eee62fe5bc
Luis Pardo <lpardosixtos@microsoft.com>
2025-10-13 23:00:04 +0000

    Deleted old versions

--- resources/todomvc/vanilla-examples/javascript-web-components-old/src/components/todo-list/todo-list.template.js
+++
@@ -1,8 +0,0 @@
-const template = document.createElement("template");
-
-template.id = "todo-list-template";
-template.innerHTML = `
-    <ul class="todo-list" style="display:none"></ul>
-`;
-
-export default template;
