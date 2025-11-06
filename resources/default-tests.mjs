191e9b71fdd32128ccb914307f77db1d6b7476dd
Luis Pardo <lpardosixtos@microsoft.com>
2025-11-05 22:38:42 -0800

    Fix bad merge

--- resources/default-tests.mjs
+++ resources/default-tests.mjs
@@ -5,67 +5,6 @@ import { freezeSuites } from "../resources/suites-helper.mjs";
 
 export const DefaultSuites = freezeSuites([
     {
-        name: "TodoMVC-LocalStorage",
-        url: "experimental/todomvc-localstorage/dist/index.html",
-        tags: ["todomvc"],
-        async prepare(page) {
-            (await page.waitForElement(".new-todo")).focus();
-            page.getLocalStorage().getItem("javascript-es5");
-        },
-        tests: [
-            new BenchmarkTestStep(`Adding${numberOfItemsToAdd}Items`, (page) => {
-                const newTodo = page.querySelector(".new-todo");
-                for (let i = 0; i < numberOfItemsToAdd; i++) {
-                    newTodo.setValue(getTodoText("ja", i));
-                    newTodo.dispatchEvent("change");
-                    newTodo.enter("keypress");
-                }
-            }),
-            new BenchmarkTestStep("CompletingAllItems", (page) => {
-                const checkboxes = page.querySelectorAll(".toggle");
-                for (let i = 0; i < numberOfItemsToAdd; i++)
-                    checkboxes[i].click();
-            }),
-            new BenchmarkTestStep("DeletingAllItems", (page) => {
-                const deleteButtons = page.querySelectorAll(".destroy");
-                for (let i = numberOfItemsToAdd - 1; i >= 0; i--)
-                    deleteButtons[i].click();
-            }),
-        ],
-    },
-    {
-        name: "TodoMVC-Emoji",
-        url: "resources/todomvc/vanilla-examples/javascript-web-components/dist/index.html",
-        tags: ["todomvc", "experimental"],
-        async prepare(page) {
-            await page.waitForElement("todo-app");
-        },
-        tests: [
-            new BenchmarkTestStep(`Adding${numberOfItemsToAdd}Items`, (page) => {
-                const input = page.querySelector(".new-todo-input", ["todo-app", "todo-topbar"]);
-                for (let i = 0; i < numberOfItemsToAdd; i++) {
-                    input.setValue(getTodoText("emoji", i));
-                    input.dispatchEvent("input");
-                    input.enter("keyup");
-                }
-            }),
-            new BenchmarkTestStep("CompletingAllItems", (page) => {
-                const items = page.querySelectorAll("todo-item", ["todo-app", "todo-list"]);
-                for (let i = 0; i < numberOfItemsToAdd; i++) {
-                    const item = items[i].querySelectorInShadowRoot(".toggle-todo-input");
-                    item.click();
-                }
-            }),
-            new BenchmarkTestStep("DeletingAllItems", (page) => {
-                const items = page.querySelectorAll("todo-item", ["todo-app", "todo-list"]);
-                for (let i = numberOfItemsToAdd - 1; i >= 0; i--) {
-                    const item = items[i].querySelectorInShadowRoot(".remove-todo-button");
-                    item.click();
-                }
-            }),
-        ],
-    },
-    {
         name: "TodoMVC-JavaScript-ES5",
         url: "resources/todomvc/vanilla-examples/javascript-es5/dist/index.html",
         tags: ["default", "todomvc"],
