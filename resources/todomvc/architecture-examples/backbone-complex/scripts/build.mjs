ce86fa0e60c25000714ac25968e3d220727f41d6
Camillo Bruni <cbruni@chromium.org>
2026-06-11 10:11:30 +0200

    Use import.meta.dirname instead of url shim

--- resources/todomvc/architecture-examples/backbone-complex/scripts/build.mjs
+++ resources/todomvc/architecture-examples/backbone-complex/scripts/build.mjs
@@ -3,10 +3,7 @@
  */
 import path from "path";
 import { buildComplex } from "big-dom-generator/utils/buildComplex.mjs";
-import { fileURLToPath as _fileURLToPath } from "url";
-import { dirname as _dirname } from "path";
-const __filename = _fileURLToPath(import.meta.url);
-const __dirname = _dirname(__filename);
+const __dirname = import.meta.dirname;
 
 
 const options = {
