8636ec785b57e43be23b1ba4b6d86353ef93e6a2
Camillo Bruni <cbruni@chromium.org>
2026-07-14 00:04:56 +0200

    Revert dist/ and build/ directories to origin/main

--- suites/newssite/news-next/dist/_next/static/speedometer-build-id/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/312.js","static/css/pages/index.css","static/chunks/pages/index.js"],"/_error":["static/chunks/pages/_error.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
