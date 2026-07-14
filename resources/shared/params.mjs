8698c7839f3e9b45cccbaa4e390ca987e6bbfd6e
Camillo Bruni <cbruni@chromium.org>
2026-07-14 16:58:38 +0200

    Implement Params.prototype.isDefault() helper (#555)
    The isDefault getter is a bit more explict than checking for equalirty against defaultParams.

--- resources/shared/params.mjs
+++ resources/shared/params.mjs
@@ -211,6 +211,10 @@ export class Params {
     toSearchParams() {
         return this.toSearchParamsObject().toString();
     }
+
+    isDefault() {
+        return this === defaultParams;
+    }
 }
 
 function isValidJsonUrl(url) {
