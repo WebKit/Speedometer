770b819ed9640ad7cc9da0195dad945f0fa92a0c
Camillo Bruni <cbruni@chromium.org>
2026-07-15 10:51:10 +0200

    fx

--- tests/unittests/suites.mjs
+++ tests/unittests/suites.mjs
@@ -64,41 +64,6 @@ for (const [name, suites] of Object.entries(Suites)) {
                 expect(suite.url.length).to.be.greaterThan(0);
             });
         });
-        it("should have resources.txt listing only valid files", async function () {
-            // validating all resource files can take a bit longer than the default timeout.
-            this.timeout(10000);
-            const baseUrl = `${window.location.origin}/`;
-            const brokenResourcesList = [];
-            for (const suite of suites) {
-                if (!suite.resources)
-                    continue;
-                const resourcesUrl = new URL(suite.resources, baseUrl).href;
-                const res = await fetch(resourcesUrl);
-                if (!res.ok)
-                    throw new Error(`Failed to load resources.txt for ${suite.name} at ${resourcesUrl}`);
-
-                const text = await res.text();
-                if (text.trim().length === 0)
-                    throw new Error(`resources.txt for ${suite.name} is empty`);
-
-                const files = text.trim().split("\n");
-                for (const file of files)
-                    expect(file.trim().length).to.be.greaterThan(0, `Found empty line in resources.txt for ${suite.name}`);
-
-                await Promise.all(
-                    files.map(async (file) => {
-                        const fileUrl = new URL(file, resourcesUrl).href;
-                        const fileRes = await fetch(fileUrl, { method: "HEAD" });
-                        if (!fileRes.ok)
-                            brokenResourcesList.push(`${fileUrl} (listed in ${resourcesUrl})`);
-                        else
-                            expect(fileRes.ok).to.be(true);
-                    })
-                );
-            }
-            if (brokenResourcesList.length > 0)
-                throw new Error(`Failed to load the following resources:\n${brokenResourcesList.join("\n")}`);
-        });
     });
 }
 
