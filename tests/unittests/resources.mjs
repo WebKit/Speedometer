05c07f6f94548c6b45202be9353d70e311d3c79e
Camillo Bruni <cbruni@chromium.org>
2026-07-15 11:00:15 +0200

    cleanup

--- tests/unittests/resources.mjs
+++ tests/unittests/resources.mjs
@@ -6,58 +6,51 @@ import { DefaultSuites } from "../../suites/default-suites.mjs";
 
 const ROOT_DIR = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "../../");
 
-const Suites = {
-    ExperimentalSuites,
-    DefaultSuites,
-};
+const Suites = [...ExperimentalSuites, ...DefaultSuites];
 
-for (const [name, suites] of Object.entries(Suites)) {
-    describe(`${name}-resources`, () => {
-        it("should have resources.txt listing only valid files via local filesystem", async function () {
-            // validating all resource files can take a bit longer than the default timeout.
-            this.timeout(10000);
-            const brokenResourcesList = [];
-            for (const suite of suites) {
-                if (!suite.resources)
-                    continue;
-                const resourcesPath = path.resolve(ROOT_DIR, suite.resources);
-                let text;
-                try {
-                    text = await fs.readFile(resourcesPath, "utf-8");
-                } catch (error) {
-                    brokenResourcesList.push(`${suite.resources} (for ${suite.name}) [error: ${error.message}]`);
-                    continue;
-                }
-
-                if (text.trim().length === 0) {
-                    brokenResourcesList.push(`${suite.resources} (for ${suite.name}) [error: resources.txt is empty]`);
-                    continue;
-                }
-
-                const files = text.trim().split("\n");
-                for (const file of files)
-                    expect(file.trim().length).to.be.greaterThan(0, `Found empty line in resources.txt for ${suite.name}`);
-
-                await Promise.all(
-                    files.map(async (file) => {
-                        const cleanFile = file.trim();
-                        if (!cleanFile)
-                            return;
-                        const filePath = cleanFile.startsWith("/")
-                            ? path.join(ROOT_DIR, cleanFile)
-                            : path.resolve(path.dirname(resourcesPath), cleanFile);
-                        try {
-                            const stat = await fs.stat(filePath);
-                            if (!stat.isFile())
-                                brokenResourcesList.push(`${cleanFile} (listed in ${suite.resources}) [error: not a file]`);
-                        } catch (error) {
-                            brokenResourcesList.push(`${cleanFile} (listed in ${suite.resources}) [error: ${error.message}]`);
-                        }
-                    })
-                );
+describe("resources", () => {
+    it("should have resources.txt listing only valid files via local filesystem", async function () {
+        // validating all resource files can take a bit longer than the default timeout.
+        this.timeout(10000);
+        const brokenResourcesList = [];
+        for (const suite of Suites) {
+            if (!suite.resources)
+                continue;
+            const resourcesPath = path.resolve(ROOT_DIR, suite.resources);
+            let text;
+            try {
+                text = await fs.readFile(resourcesPath, "utf-8");
+            } catch (error) {
+                brokenResourcesList.push(`${suite.resources} (for ${suite.name}) [error: ${error.message}]`);
+                continue;
             }
-            if (brokenResourcesList.length > 0)
-                throw new Error(`Failed to check the following resources:\n${brokenResourcesList.join("\n")}`);
-        });
+
+            if (text.trim().length === 0) {
+                brokenResourcesList.push(`${suite.resources} (for ${suite.name}) [error: resources.txt is empty]`);
+                continue;
+            }
+
+            const files = text.trim().split("\n");
+            for (const file of files)
+                expect(file.trim().length).to.be.greaterThan(0, `Found empty line in resources.txt for ${suite.name}`);
+
+            await Promise.all(
+                files.map(async (file) => {
+                    const cleanFile = file.trim();
+                    if (!cleanFile)
+                        return;
+                    const filePath = cleanFile.startsWith("/") ? path.join(ROOT_DIR, cleanFile) : path.resolve(path.dirname(resourcesPath), cleanFile);
+                    try {
+                        const stat = await fs.stat(filePath);
+                        if (!stat.isFile())
+                            brokenResourcesList.push(`${cleanFile} (listed in ${suite.resources}) [error: not a file]`);
+                    } catch (error) {
+                        brokenResourcesList.push(`${cleanFile} (listed in ${suite.resources}) [error: ${error.message}]`);
+                    }
+                })
+            );
+        }
+        if (brokenResourcesList.length > 0)
+            throw new Error(`Failed to check the following resources:\n${brokenResourcesList.join("\n")}`);
     });
-}
+});
