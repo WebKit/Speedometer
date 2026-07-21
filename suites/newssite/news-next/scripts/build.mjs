84701cac8c02bbd00e53f466ed97e97e1bbfc1bc
Camillo Bruni <cbruni@chromium.org>
2026-07-21 12:25:46 +0200

    cleanup-build-scripts

--- suites/newssite/news-next/scripts/build.mjs
+++
@@ -1,4 +0,0 @@
-import { execSync } from "child_process";
-import path from "path";
-
-execSync("npm run build:next", { stdio: "inherit", cwd: path.join(import.meta.dirname, "..") });
--- suites/newssite/news-nuxt/scripts/build.mjs
+++
@@ -1,4 +0,0 @@
-import { execSync } from "child_process";
-import path from "path";
-
-execSync("npm run build:nuxt", { stdio: "inherit", cwd: path.join(import.meta.dirname, "..") });
